af3af918929be540218dccd4aff8f173acf5e5d7
Josh Goodwin <j@joshuagoodw.in>
2015-06-24 14:17:08 +0100

    show all stops in a locality on map

--- busstops/static/js/map.js
+++ busstops/static/js/map.js
@@ -1,17 +1,18 @@
-(function (locations) {
+(function (locations, bounds, center) {
     if (locations.length && document.getElementById('map').clientWidth === 460) {
         var map = L.map('map');
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
-        console.log(locations);
+
         for (var i = 0; i < locations.length; i++) {
             L.marker([locations[i][0], locations[i][1]]).addTo(map).bindPopup(locations[i][2]).openPopup();
         }
-        // if (locations.length === 1) {
+
+        if (bounds && center) {
+            map.fitBounds(bounds).setView(center);
+        } else {
             map.setView([locations[0][0], locations[0][1]], 17);
-        // } else {
-            
-        // }
+        }
     }
-})(mapLocations);
+})(mapOptions.locations, mapOptions.bounds || false, mapOptions.center || false);
