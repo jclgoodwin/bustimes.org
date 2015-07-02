d175cc33a99316796499aad5e31b0231542abff3
Josh Goodwin <j@joshuagoodw.in>
2015-07-02 23:56:50 +0100

    ditch emojis

--- busstops/static/js/locate.js
+++ busstops/static/js/locate.js
@@ -7,9 +7,9 @@
                 navigator.geolocation.getCurrentPosition(function (p) {
                     var x = Math.round(p.coords.longitude * 1000 ) / 1000,
                         y = Math.round(p.coords.latitude * 1000 ) / 1000;
-                    buttons[0].className = 'loading';
+                    buttons[0].className += ' loading';
                     buttons[0].setAttribute('disabled', 'disabled')
-                    buttons[0].innerHTML = 'Finding bus stops near ' + y + ', ' + x;
+                    buttons[0].innerHTML = 'Finding bus stops near ' + y + ', ' + x + '\u2026';
                     window.location.href = '/coordinates/' + y + ',' + x;
                 });
         }
