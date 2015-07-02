9d4bf8b004f0452a0eb67c99cac24b92d48f3c3a
Josh Goodwin <j@joshuagoodw.in>
2015-07-03 00:07:36 +0100

    admin register more models

--- busstops/admin.py
+++ busstops/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from busstops.models import *
 
 
-admin.site.register(StopPoint)
-admin.site.register(Locality)
+admin.site.register(Region)
 admin.site.register(AdminArea)
+admin.site.register(District)
+admin.site.register(Locality)
+admin.site.register(StopPoint)
+admin.site.register(Operator)
+admin.site.register(Service)
+admin.site.register(ServiceVersion)
+# admin.site.register(JourneyPatternSection)
+# admin.site.register(JourneyPatternTimingLink)
+# admin.site.register(OperatingProfile)
+# admin.site.register(VehicleJourney)
