e12e669acdc216a42394f6ea4c704b785a32aafe
Josh Goodwin <j@joshuagoodw.in>
2015-12-28 18:42:50 +0000

    Silence import.sh if there's nothing new to import

--- data/import.sh
+++ data/import.sh
@@ -12,25 +12,23 @@ PASSWORD=$2
 REGIONS=(EA EM L NE NW S SE SW W WM Y NCSD)
 . ../env/bin/activate
 
+# set $md5 to the name of the system's md5 hash command ('md5' or 'md5sum')
 md5=$(which md5)
-if [[ ! "${md5}" ]]
-then
+if [[ ! "${md5}" ]] then
     md5=$(which md5sum)
-    if [[ ! "${md5}" ]]
-    then
+    if [[ ! "${md5}" ]] then
         echo "Neither md5 nor md5sum found :("
         exit 1
     fi
 fi
 
-echo "NPTG"
 cd NPTG
 nptg_md5_old=`$md5 nptgcsv.zip`
 wget -qN http://81.17.70.199/nptg/snapshot/nptgcsv.zip
 nptg_md5_new=`$md5 nptgcsv.zip`
 
-if [[ $nptg_md5_old != $nptg_md5_new ]]
-then
+if [[ $nptg_md5_old != $nptg_md5_new ]] then
+    echo "NPTG"
     echo "  Changes found"
     echo "  Unzipping"
     unzip -oq nptgcsv.zip
@@ -40,14 +38,13 @@ then
     ../../manage.py import_locality_hierarchy < LocalityHierarchy.csv
 fi
 
-echo "NaPTAN"
 cd ../NaPTAN
 naptan_md5_old=`$md5 NaPTANcsv.zip`
 wget -qN http://81.17.70.199/NaPTAN/snapshot/NaPTANcsv.zip
 naptan_md5_new=`$md5 NaPTANcsv.zip`
 
-if [[ $nptg_md5_old != $nptg_md5_new || $naptan_md5_old != $naptan_md5_new ]]
-then
+if [[ $nptg_md5_old != $nptg_md5_new || $naptan_md5_old != $naptan_md5_new ]] then
+    echo "NaPTAN"
     echo "  Changes found"
     echo "  Unzipping"
     unzip -oq NaPTANcsv.zip
@@ -66,21 +63,22 @@ then
     ../../manage.py import_stops_in_area < StopsInArea.csv
 fi
 
-echo "Operators"
 cd ..
+noc_md5_old=`$md5 NOC_DB.csv`
 wget -qN http://mytraveline.info/NOC/NOC_DB.csv
-../manage.py import_operators < NOC_DB.csv
+noc_md5_new=`$md5 NOC_DB.csv`
+if [[ $noc_md5_old != $noc_md5_new ]] then
+    ../manage.py import_operators < NOC_DB.csv
+fi
 
-echo "Services"
 cd TNDS
 for region in ${REGIONS[@]}
 do
     region_md5_old=`$md5 $region.zip`
     wget -qN --user=$USERNAME --password=$PASSWORD ftp://ftp.tnds.basemap.co.uk/$region.zip
     region_md5_new=`$md5 $region.zip`
-    if [[ $nptg_md5_old != $nptg_md5_new || $naptan_md5_old != $naptan_md5_new || $region_md5_old != $region_md5_new ]]
-    then
-        echo "  "$region
+    if [[ $nptg_md5_old != $nptg_md5_new || $naptan_md5_old != $naptan_md5_new || $region_md5_old != $region_md5_new ]] then
+        echo $region
         # (
         ../../manage.py import_services $region.zip
         # ) &
