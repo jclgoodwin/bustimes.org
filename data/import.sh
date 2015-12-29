6a5250c1cba4253994da07dcb702e2ccdd1e2713
Josh Goodwin <j@joshuagoodw.in>
2015-12-29 18:20:41 +0000

    Fix import.sh syntax errors

--- data/import.sh
+++ data/import.sh
@@ -14,9 +14,9 @@ REGIONS=(EA EM L NE NW S SE SW W WM Y NCSD)
 
 # set $md5 to the name of the system's md5 hash command ('md5' or 'md5sum')
 md5=$(which md5)
-if [[ ! "${md5}" ]] then
+if [[ ! "${md5}" ]]; then
     md5=$(which md5sum)
-    if [[ ! "${md5}" ]] then
+    if [[ ! "${md5}" ]]; then
         echo "Neither md5 nor md5sum found :("
         exit 1
     fi
@@ -27,7 +27,7 @@ nptg_md5_old=`$md5 nptgcsv.zip`
 wget -qN http://81.17.70.199/nptg/snapshot/nptgcsv.zip
 nptg_md5_new=`$md5 nptgcsv.zip`
 
-if [[ $nptg_md5_old != $nptg_md5_new ]] then
+if [[ $nptg_md5_old != $nptg_md5_new ]]; then
     echo "NPTG"
     echo "  Changes found"
     echo "  Unzipping"
@@ -43,7 +43,7 @@ naptan_md5_old=`$md5 NaPTANcsv.zip`
 wget -qN http://81.17.70.199/NaPTAN/snapshot/NaPTANcsv.zip
 naptan_md5_new=`$md5 NaPTANcsv.zip`
 
-if [[ $nptg_md5_old != $nptg_md5_new || $naptan_md5_old != $naptan_md5_new ]] then
+if [[ $nptg_md5_old != $nptg_md5_new || $naptan_md5_old != $naptan_md5_new ]]; then
     echo "NaPTAN"
     echo "  Changes found"
     echo "  Unzipping"
@@ -67,17 +67,16 @@ cd ..
 noc_md5_old=`$md5 NOC_DB.csv`
 wget -qN http://mytraveline.info/NOC/NOC_DB.csv
 noc_md5_new=`$md5 NOC_DB.csv`
-if [[ $noc_md5_old != $noc_md5_new ]] then
+if [[ $noc_md5_old != $noc_md5_new ]]; then
     ../manage.py import_operators < NOC_DB.csv
 fi
 
 cd TNDS
-for region in ${REGIONS[@]}
-do
+for region in ${REGIONS[@]}; do
     region_md5_old=`$md5 $region.zip`
     wget -qN --user=$USERNAME --password=$PASSWORD ftp://ftp.tnds.basemap.co.uk/$region.zip
     region_md5_new=`$md5 $region.zip`
-    if [[ $nptg_md5_old != $nptg_md5_new || $naptan_md5_old != $naptan_md5_new || $region_md5_old != $region_md5_new ]] then
+    if [[ $nptg_md5_old != $nptg_md5_new || $naptan_md5_old != $naptan_md5_new || $region_md5_old != $region_md5_new ]]; then
         echo $region
         # (
         ../../manage.py import_services $region.zip
