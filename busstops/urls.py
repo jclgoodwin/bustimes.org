ce3abb5df6dfc2a4ba812fe22e9d3e59d91eed94
Josh Goodwin <j@joshuagoodw.in>
2016-05-01 15:57:29 +0100

    Remove old search view

--- busstops/urls.py
+++ busstops/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     url(r'^data/?$', views.data),
     url(r'^map/?$', views.hugemap),
     url(r'^stops.json', views.stops),
-    url(r'^search.json', views.search),
     url(r'^regions/(?P<pk>[A-Z]+)/?$', views.RegionDetailView.as_view(), name='region-detail'),
     url(r'^(admin-)?areas/(?P<pk>\d+)/?$', views.AdminAreaDetailView.as_view(), name='adminarea-detail'),
     url(r'^districts/(?P<pk>\d+)/?$', views.DistrictDetailView.as_view(), name='district-detail'),
