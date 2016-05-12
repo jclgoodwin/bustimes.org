05fb9a19edb5c4d9182a424afc41484f79b7d03d
Josh Goodwin <j@joshuagoodw.in>
2016-05-12 21:10:47 +0100

    More contact form

--- busstops/forms.py
+++ busstops/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Name', required=False)
-    email = forms.EmailField(label='Email address', required=False)
-    message = forms.CharField(label='Message', required=False, widget=forms.Textarea)
+    name = forms.CharField(label='Name')
+    email = forms.EmailField(label='Email address')
+    message = forms.CharField(label='Message', widget=forms.Textarea)
     referrer = forms.CharField(label='Referrer', required=False, widget=forms.HiddenInput)
