e808bfc2104689f93c1d255c4933fd95a8ec5b88
Josh Goodwin <j@joshuagoodw.in>
2016-05-22 11:53:41 +0100

    Tweak not found redirect middleware

--- buses/middleware.py
+++ buses/middleware.py
@@ -19,14 +19,26 @@ class NotFoundRedirectMiddleware(object):
     def process_response(self, request, response):
         if response.status_code == 404:
             suggestion = None
-            if request.path.startswith('/services/') and len(request.path.split('-')) >= 4:
-                service_code_parts = request.path.split('/')[-1].split('-')[:4]
-                suggestion = Service.objects.filter(
-                    service_code__icontains='-'.join(service_code_parts),
-                    current=True
-                ).first()
+
+            if request.path.startswith('/services/'):
+                service_code = request.path.split('/')[-1]
+                service_code_parts = service_code.split('-')
+
+                if len(service_code_parts) >= 4:
+                    suggestion = Service.objects.filter(
+                        service_code__icontains='_' + '-'.join(service_code_parts[:4]),
+                        current=True
+                    ).first()
+                if suggestion is None:
+                    suggestion = Service.objects.filter(
+                        service_code__iexact=service_code,
+                        current=True
+                    ).first()
+
             elif request.path.startswith('/stops/'):
                 suggestion = StopPoint.objects.only('atco_code').filter(naptan_code=request.path.split('/')[-1]).first()
+
             if suggestion is not None:
                 return redirect(suggestion)
+
         return response
