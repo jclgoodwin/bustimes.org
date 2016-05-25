32d9f627e9d592a693e7cc3e778463ebb6dd796d
Josh Goodwin <j@joshuagoodw.in>
2016-05-25 23:22:47 +0100

    Fix rough command for correcting operator regions

--- busstops/management/commands/correct_operator_regions.py
+++ busstops/management/commands/correct_operator_regions.py
@@ -4,7 +4,7 @@ from busstops.models import Operator, Region
 
 class Command(BaseCommand):
     @staticmethod
-    def maybe_move_operator(operator, regions)
+    def maybe_move_operator(operator, regions):
         if bool(regions) and operator.region not in regions:
             if len(regions) == 1:
                 print 'moving', operator, 'from', operator.region, 'to', regions[0]
@@ -17,9 +17,9 @@ class Command(BaseCommand):
         for operator in Operator.objects.all():
             # move Anglian Bus to the East Anglia
             regions = Region.objects.filter(service__operator=operator).distinct()
-            maybe_move_operator(operator, regions)
+            self.maybe_move_operator(operator, regions)
 
             # move Cumbria to the North West
             regions = Region.objects.filter(adminarea__locality__stoppoint__service__operator=operator).distinct()
-            maybe_move_operator(operator, regions)
+            self.maybe_move_operator(operator, regions)
 
