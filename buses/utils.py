eff3195097e9599b87f5cec9bbae744b91ae16cf
Josh Goodwin <j@joshuagoodw.in>
2016-05-25 19:04:39 +0100

    Add custom Hastack get_identifier function

--- buses/utils.py
+++ buses/utils.py
@@ -1,7 +1,13 @@
 import re
+from haystack.utils import default_get_identifier
 
 
 def minify(template_source):
     template_source = re.sub(r'(\n *)+', '\n', template_source)
     template_source = re.sub(r'({%.+%})\n+', r'\1', template_source)
     return template_source
+
+def get_identifier(obj_or_string):
+    if isinstance(obj_or_string, basestring):
+        return obj_or_string
+    return default_get_identifier(obj_or_string)
