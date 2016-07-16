f8223e09b84f169eb3063fd8ad4e7fe0a1e02be3
Josh Goodwin <j@joshuagoodw.in>
2016-07-16 12:42:27 +0100

    Clean stops a bit during import

--- busstops/management/commands/import_stops.py
+++ busstops/management/commands/import_stops.py
@@ -5,8 +5,15 @@ Usage:
 """
 
 from django.contrib.gis.geos import Point
+from titlecase import titlecase
+from . import clean_stops
 from ..import_from_csv import ImportFromCSVCommand
 from ...models import StopPoint
+
+
+INDICATORS_TO_PROPER_CASE = {
+    indicator.lower(): indicator for indicator in clean_stops.INDICATORS_TO_PROPER_CASE
+}
 
 
 class Command(ImportFromCSVCommand):
@@ -23,8 +30,24 @@ class Command(ImportFromCSVCommand):
             'active': (row['Status'] == 'act'),
             'admin_area_id': row['AdministrativeAreaCode']
         }
+
         for django_field_name, naptan_field_name in self.field_names:
-            defaults[django_field_name] = unicode(row[naptan_field_name], errors='ignore')
+            value = unicode(row[naptan_field_name], errors='ignore').strip()
+            if django_field_name in ('street', 'crossing', 'landmark'):
+                if value in ('-', '--', '---', '*', 'TBA', 'unknown'):
+                    value = ''
+                elif value.isupper():
+                    value = titlecase(value)
+            defaults[django_field_name] = value
+
+        if defaults['indicator'] in clean_stops.INDICATORS_TO_REPLACE:
+            defaults['indicator'] = clean_stops.INDICATORS_TO_REPLACE.get(
+                defaults['indicator']
+            )
+        elif defaults['indicator'].lower() in INDICATORS_TO_PROPER_CASE:
+            defaults['indicator'] = INDICATORS_TO_PROPER_CASE.get(
+                defaults['indicator'].lower()
+            )
 
         StopPoint.objects.update_or_create(atco_code=row['ATCOCode'], defaults=defaults)
 
