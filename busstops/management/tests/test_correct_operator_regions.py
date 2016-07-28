6b66fb906ee18a77166b5a66d8d9b787cd702382
Josh Goodwin <j@joshuagoodw.in>
2016-07-28 17:01:12 +0100

    Fix flake

--- busstops/management/tests/test_correct_operator_regions.py
+++ busstops/management/tests/test_correct_operator_regions.py
@@ -8,42 +8,41 @@ class CorretcOperatorRegionsTest(TestCase):
     def setUpTestData(cls):
         cls.command = correct_operator_regions.Command()
 
-        cls.east_midlands = Region.objects.create(id='EM', name='East Midlands')
-        cls.west_midlands = Region.objects.create(id='WM', name='West Midlands')
-        cls.middle_east = Region.objects.create(id='ME', name='Middle East')
+        cls.east = Region.objects.create(id='E', name='East')
+        cls.west = Region.objects.create(id='W', name='West')
+        cls.north = Region.objects.create(id='N', name='North')
 
         cls.goodwins = Operator.objects.create(
-            region=cls.east_midlands,
+            region=cls.east,
             pk='GDWN',
             name='Go Goodwins'
         )
         cls.tellings = Operator.objects.create(
-            region=cls.east_midlands,
+            region=cls.east,
             pk='TGML',
             name='Tellings Golden Miller'
         )
 
         cls.west_midlands_service = Service.objects.create(
             service_code='1',
-            region=cls.west_midlands,
+            region=cls.west,
             date='2016-05-05'
         )
         cls.west_midlands_service.operator.set([cls.goodwins])
 
     def test_handle(self):
-        self.assertEqual(self.goodwins.region_id, 'EM')
-        self.assertEqual(self.tellings.region_id, 'EM')
+        self.assertEqual(self.goodwins.region_id, 'E')
+        self.assertEqual(self.tellings.region_id, 'E')
 
         self.command.handle()
 
-        self.assertEqual(Operator.objects.get(id='GDWN').region_id, 'WM')
-        self.assertEqual(Operator.objects.get(id='TGML').region_id, 'EM')
+        self.assertEqual(Operator.objects.get(id='GDWN').region_id, 'W')
+        self.assertEqual(Operator.objects.get(id='TGML').region_id, 'E')
 
     def test_maybe(self):
-        self.assertEqual(self.tellings.region_id, 'EM')
+        self.assertEqual(self.tellings.region_id, 'E')
 
         self.assertEqual(
-            'consider moving Tellings Golden Miller from East Midlands to [<Region: Middle East>, <Region: West Midlands>]',
-            self.command.maybe_move_operator(self.tellings, [self.middle_east, self.west_midlands])
+            'consider moving Tellings Golden Miller from East to [<Region: West>, <Region: North>]',
+            self.command.maybe_move_operator(self.tellings, [self.west, self.north])
         )
-
