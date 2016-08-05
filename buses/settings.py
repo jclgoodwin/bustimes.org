c7176a0ce2222b7be97317a21812ab555a33463f
Josh Goodwin <j@joshuagoodw.in>
2016-08-05 22:14:45 +0100

    Fix Travis/debug toolbar

--- buses/settings.py
+++ buses/settings.py
@@ -26,19 +26,34 @@ INSTALLED_APPS = [
     'busstops',
     'pipeline',
 ]
-if DEBUG and 'test' not in sys.argv and not bool(os.environ.get('TRAVIS')):
-    INSTALLED_APPS += ['debug_toolbar']
-    DEBUG_TOOLBAR_PANELS = ['debug_toolbar.panels.profiling.ProfilingPanel']
 
-ROOT_URLCONF = 'buses.urls'
-
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'busstops.middleware.NotFoundRedirectMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
-)
+]
+
+if DEBUG and 'test' not in sys.argv and not bool(os.environ.get('TRAVIS')):
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE_CLASSES += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel'
+    ]
+
+ROOT_URLCONF = 'buses.urls'
 
 HAYSTACK_CONNECTIONS = {
     'default': {
