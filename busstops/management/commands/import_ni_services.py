b2e22dbb9af80825964fa26a50d8ad7872d2389d
Josh Goodwin <j@joshuagoodw.in>
2016-12-11 20:50:02 +0000

    Test and change import_ni_services

--- busstops/management/commands/import_ni_services.py
+++ busstops/management/commands/import_ni_services.py
@@ -11,6 +11,8 @@ class Command(BaseCommand):
     services = {}
     deferred_stop_codes = []
     deferred_stops = {}
+    direction = None
+    service_code = None
 
     @staticmethod
     def get_file_header(line):
@@ -31,15 +33,80 @@ class Command(BaseCommand):
             'direction': line[64:],
         }
 
-    @staticmethod
-    def get_route_description(line):
-        return {
-            'transaction_type': line[2:3],
-            'operator': line[3:7],
-            'route_number': line[7:11],
-            'route_direction': line[11:12],
-            'route_description': line[12:]
-        }
+    @classmethod
+    def handle_route_description(cls, line):
+        assert line[2:3] == 'N'
+
+        operator = line[3:7].strip().upper()
+        route_number = line[7:11].strip()
+        # route_direction = line[11:12]
+        route_description = line[12:].strip()
+        service_code = route_number + '_' + operator
+        cls.service_code = service_code
+
+        if service_code not in cls.services:
+            cls.services[service_code] = {'O': {}, 'I': {}}
+            defaults = {
+                'region_id': 'NI',
+                'date': '2016-11-01',
+                'line_name': route_number,
+                'description': route_description,
+                'mode': 'bus'
+            }
+            if operator:
+                defaults['operator'] = [operator]
+            Service.objects.update_or_create(service_code=service_code, defaults=defaults)
+
+    @classmethod
+    def handle_stop(cls, line):
+        record_identity = line[:2]
+        atco_code = line[2:14]
+        if atco_code not in cls.services[cls.service_code][cls.direction]:
+            if not StopPoint.objects.filter(atco_code=atco_code).exists():
+                print(atco_code)
+                cls.deferred_stop_codes.append(atco_code)
+                return
+            if record_identity == 'QI':
+                timing_status = line[26:28]
+                order = 1
+            else:
+                timing_status = line[21:23]
+                if record_identity == 'QO':
+                    order = 0
+                else:
+                    order = 2
+            cls.services[cls.service_code][cls.direction][atco_code] = StopUsage(
+                service_id=cls.service_code,
+                stop_id=atco_code,
+                direction=('Outbound' if cls.direction == 'O' else 'Inbound'),
+                timing_status=('PTP' if timing_status == 'T1' else 'OTH'),
+                order=order
+            )
+
+    @classmethod
+    def handle_location(cls, line):
+        atco_code = line[3:15]
+        if atco_code in cls.deferred_stop_codes:
+            common_name = line[15:].strip()
+            cls.deferred_stops[atco_code] = StopPoint(
+                atco_code=atco_code,
+                common_name=common_name
+            )
+
+    @classmethod
+    def handle_location_additional(cls, line):
+        atco_code = line[3:15],
+        if atco_code in cls.deferred_stops:
+            assert atco_code in cls.deferred_stops
+            latlong = Point(
+                int(line[15:23].strip()),
+                int(line[23:].strip()),
+                srid=29902  # Irish Grid
+            )
+            cls.deferred_stops[atco_code].active = True
+            cls.deferred_stops[atco_code].locality_centre = False
+            cls.deferred_stops[atco_code].latlong = latlong
+            cls.deferred_stops[atco_code].save()
 
     @staticmethod
     def get_journey_note(line):
@@ -48,91 +115,29 @@ class Command(BaseCommand):
             'note_text': line[7:],
         }
 
-    @staticmethod
-    def get_location(line):
-        return {
-            'atco_code': line[3:15],
-            'common_name': line[15:].strip()
-        }
-
-    @staticmethod
-    def get_location_additional(line):
-        return {
-            'atco_code': line[3:15],
-            'easting': line[15:23].strip(),
-            'northing': line[23:].strip()
-        }
+    @classmethod
+    def handle_line(cls, line):
+        record_identity = line[:2]
+        # QS - Journey Header
+        if record_identity == 'QS':
+            cls.direction = cls.get_journey_header(line)['direction'].strip()
+        # QD - Route Description
+        elif record_identity == 'QD':
+            cls.handle_route_description(line)
+        # QO - Journey Origin
+        # QI - Journey Intermediate
+        # QT - Journey Destination
+        elif record_identity in ('QO', 'QI', 'QT'):
+            cls.handle_stop(line)
+        elif record_identity == 'QL':
+            cls.handle_location(line)
+        elif record_identity == 'QB':
+            cls.handle_location_additional(line)
 
     @classmethod
     def handle_open_file(cls, open_file):
         for line in open_file:
-            record_identity = line[:2]
-            # QS - Journey Header
-            if record_identity == 'QS':
-                direction = cls.get_journey_header(line)['direction'].strip()
-            # QD - Route Description
-            elif record_identity == 'QD':
-                service = cls.get_route_description(line)
-                operator = service['operator'].strip().upper()
-                route_number = service['route_number'].strip()
-                service_code = route_number + '_' + operator
-                if service_code not in cls.services:
-                    cls.services[service_code] = {'O': {}, 'I': {}}
-                    try:
-                        Service.objects.update_or_create(
-                            service_code=service_code,
-                            defaults={
-                                'region_id': 'NI',
-                                'date': '2016-11-01',
-                                'line_name': line[7:11].strip(),
-                                'description': service['route_description'].strip(),
-                                'operator': [operator] if operator else None,
-                                'mode': 'bus'
-                            }
-                        )
-                    except Exception as e:
-                        print(e)
-            # QO - Journey Origin
-            # QI - Journey Intermediate
-            # QT - Journey Destination
-            elif record_identity in ('QO', 'QI', 'QT'):
-                atco_code = line[2:14]
-                if atco_code not in cls.services[service_code][direction]:
-                    if not StopPoint.objects.filter(atco_code=atco_code).exists():
-                        print(atco_code)
-                        cls.deferred_stop_codes.append(atco_code)
-                        continue
-                    if record_identity == 'QI':
-                        timing_status = line[26:28]
-                        order = 1
-                    else:
-                        timing_status = line[21:23]
-                        if record_identity == 'QO':
-                            order = 0
-                        else:
-                            order = 2
-                    cls.services[service_code][direction][atco_code] = StopUsage.objects.create(
-                        service_id=service_code,
-                        stop_id=atco_code,
-                        direction=('Outbound' if direction == 'O' else 'Inbound'),
-                        timing_status=('PTP' if timing_status == 'T1' else 'OTH'),
-                        order=order
-                    )
-            elif record_identity == 'QL':
-                location = cls.get_location(line)
-                if location['atco_code'] in cls.deferred_stop_codes:
-                    cls.deferred_stops[location['atco_code']] = StopPoint(**location)
-            elif record_identity == 'QB':
-                location_additional = cls.get_location_additional(line)
-                if location_additional['atco_code'] in cls.deferred_stop_codes:
-                    cls.deferred_stops[location['atco_code']].active = True
-                    cls.deferred_stops[location['atco_code']].locality_centre = False
-                    cls.deferred_stops[location_additional['atco_code']].latlong = Point(
-                        int(location_additional['easting']),
-                        int(location_additional['northing']),
-                        srid=29902  # Irish Grid
-                    )
-                    cls.deferred_stops[location_additional['atco_code']].save()
+            cls.handle_line(line)
 
     @classmethod
     def handle_file(cls, path):
--- busstops/management/tests/test_import_ni.py
+++ busstops/management/tests/test_import_ni.py
@@ -2,7 +2,7 @@
 """
 import os
 from django.test import TestCase
-from ...models import StopPoint
+from ...models import StopPoint, Region, Operator, Service
 from ..commands import import_ni_stops, import_ni_services
 from .test_import_nptg import ImportNPTGTest
 
@@ -32,7 +32,12 @@ class ImportNIServcesTest(TestCase):
     """
     command = import_ni_services.Command()
 
-    def test_get_file_header(self):
+    @classmethod
+    def setUpTestData(cls):
+        cls.norn_iron = Region.objects.create(id='NI', name='Northern Ireland')
+        cls.metro = Operator.objects.create(id='MET', name='Translink Metro', region_id='NI')
+
+    def test_file_header(self):
         line = 'ATCO-CIF0500Metro                           OMNITIMES       20160802144451\n'
         self.assertEqual(self.command.get_file_header(line), {
             'file_type': 'ATCO-CIF',
@@ -42,17 +47,7 @@ class ImportNIServcesTest(TestCase):
             'production_datetime': '20160802144451'
         })
 
-    def test_get_route_description(self):
-        line = 'QDNMET 1A  OCity Centre - Carnmoney - Fairview Road - Glenville                 \n'
-        self.assertEqual(self.command.get_route_description(line), {
-            'transaction_type': 'N',
-            'operator': 'MET ',
-            'route_number': '1A  ',
-            'route_direction': 'O',
-            'route_description': 'City Centre - Carnmoney - Fairview Road - Glenville                 \n'
-        })
-
-    def test_get_journey_header(self):
+    def test_journey_header(self):
         line = 'QSNMET 0510  20160901201706301111100 X1A                        O\n'
         self.assertEqual(self.command.get_journey_header(line), {
             'transaction_type': 'N',
@@ -60,3 +55,24 @@ class ImportNIServcesTest(TestCase):
             'unique_journey_identifier': '0510  ',
             'direction': 'O\n',
         })
+        self.assertIsNone(self.command.direction)
+        self.command.handle_line(line)
+        self.assertEqual(self.command.direction, 'O')
+
+    def test_route_description(self):
+        self.assertEqual(len(Service.objects.filter(service_code='1A_MET')), 0)
+        self.assertIsNone(self.command.service_code)
+        self.assertEqual(self.command.services, {})
+
+        line = 'QDNMET 1A  OCity Centre - Carnmoney - Fairview Road - Glenville                 \n'
+        self.command.handle_line(line)
+
+        service = Service.objects.get(service_code='1A_MET')
+        self.assertEqual(service.line_name, '1A')
+        self.assertEqual(service.description, 'City Centre - Carnmoney - Fairview Road - Glenville')
+        self.assertEqual(self.command.service_code, '1A_MET')
+        self.assertEqual(self.command.services, {'1A_MET': {'I': {}, 'O': {}}})
+
+    def test_stop(self):
+        self.command.handle_line('QO7000000153640545UQST1  \n')
+        self.assertEqual(self.command.deferred_stop_codes, ['700000015364'])
