0375c95e9787101e3b669d231837001799ca459b
Josh Goodwin <j@joshuagoodw.in>
2017-01-28 16:37:13 +0000

    Hush cache warming command (the emails were crashing my email client)

--- busstops/management/commands/warm_cache.py
+++ busstops/management/commands/warm_cache.py
@@ -1,4 +1,3 @@
-import time
 from django.core.management.base import BaseCommand
 from ...models import Service
 from ...utils import timetable_from_service
@@ -7,8 +6,4 @@ from ...utils import timetable_from_service
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for service in Service.objects.filter(current=True):
-            print(service)
-            start = time.time()
             timetable_from_service(service)
-            end = time.time()
-            print(end - start)
