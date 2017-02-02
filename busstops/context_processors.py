6dc8ad79d436b45fc1fff1750a9f2aeaa10845a6
Josh Goodwin <j@joshuagoodw.in>
2017-02-02 21:53:08 +0000

    Stop Clickio experiment

--- busstops/context_processors.py
+++ busstops/context_processors.py
@@ -1,13 +1,4 @@
-from random import getrandbits
-
-
 def amp(request):
     return {
         'amp': 'amp' in request.GET
-    }
-
-
-def random(_):
-    return {
-        'random': bool(getrandbits(1))
     }
