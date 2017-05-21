9bf7f7267e5b28e7212ecdf3d616d65ecb707ce1
Josh Goodwin <j@joshuagoodw.in>
2017-05-21 19:39:17 +0100

    Add stops and proper regions and stuff to Irish services

--- busstops/management/commands/import_ie_gtfs.py
+++ busstops/management/commands/import_ie_gtfs.py
@@ -3,15 +3,17 @@ import time
 import zipfile
 import csv
 import requests
-# from django.contrib.gis.geos import LineString, MultiLineString
+from django.contrib.gis.geos import Point
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from ...models import Operator, Service
+from django.db.models import Count
+from ...models import Operator, Service, StopPoint, StopUsage, Region
 
 
 MODES = {
     '2': 'rail',
-    '3': 'bus'
+    '3': 'bus',
+    '4': 'ferry'
 }
 COLLECTIONS = (
     'luasbus', 'dublinbus', 'kenneallys', 'locallink', 'irishrail', 'ferries',
@@ -41,12 +43,23 @@ class Command(BaseCommand):
     # def add_arguments(parser):
     #     parser.add_argument('filenames', nargs='+', type=str)
 
-    @transaction.atomic
-    def handle_zipfile(self, archive_name):
+    def handle_zipfile(self, archive_name, collection):
         routes = {}
         trips = {}
         agencies = {}
+        operators = set()
         with zipfile.ZipFile(archive_name) as archive:
+            with archive.open('stops.txt') as csv_file:
+                for row in get_rows(csv_file):
+                    if not (len(row['stop_id']) > 16 or StopPoint.objects.filter(atco_code=row['stop_id']).exists()):
+                        StopPoint.objects.create(
+                            atco_code=row['stop_id'],
+                            latlong=Point(float(row['stop_lon']), float(row['stop_lat'])),
+                            common_name=row['stop_name'][:48],
+                            locality_centre=False,
+                            admin_area_id=row['stop_id'][:3],
+                            active=True
+                        )
             with archive.open('agency.txt') as csv_file:
                 for row in get_rows(csv_file):
                     agencies[row['agency_id']] = row
@@ -67,26 +80,80 @@ class Command(BaseCommand):
             defaults = {
                 'region_id': 'LE',
                 'line_name': route['route_short_name'],
+                'description': route['route_long_name'],
                 'date': '2017-01-01',
+                'mode': MODES.get(route['route_type'], '')
             }
             if route['trips']:
-                defaults['description'] = route['trips'][0]['trip_headsign']
+                for trip in route['trips']:
+                    if not defaults['description']:
+                        defaults['description'] = trip['trip_headsign'].strip()
+                    if trip['direction_id'] == '0' and not defaults.get('outbound_description'):
+                        defaults['outbound_description'] = trip['trip_headsign'].strip()
+                    elif trip['direction_id'] == '1' and not defaults.get('inbound_description'):
+                        defaults['inbound_description'] = trip['trip_headsign'].strip()
             else:
-                print(route)
                 break
-            if route['route_type'] in MODES:
-                defaults['mode'] = MODES[route['route_type']]
-            service = Service.objects.update_or_create(service_code=route['route_id'], defaults=defaults)[0]
-            if route['agency_id']:
-                if Operator.objects.filter(name=agencies[route['agency_id']]['agency_name']).exists():
-                    service.operator.add(Operator.objects.get(name=agencies[route['agency_id']]['agency_name']))
-                else:
-                    print(agencies[route['agency_id']]['agency_name'])
 
+            service, created = Service.objects.update_or_create(
+                service_code='{}-{}'.format(collection, '-'.join(route['route_id'].split('-')[:-1])),
+                defaults=defaults
+            )
+            # if not created:
+            #     print(service)
+            if route['agency_id']:
+                operator = Operator.objects.get_or_create(name=agencies[route['agency_id']]['agency_name'], defaults={
+                    'id': route['agency_id'],
+                    'region_id': 'LE',
+                    'vehicle_mode': defaults['mode']
+                })[0]
+                service.operator.add(operator)
+                operators.add(operator)
+
+            outbound_stops = {}
+            inbound_stops = {}
+            for trip in route['trips']:
+                if trip['direction_id'] == '0':
+                    stops = outbound_stops
+                    direction = 'Outbound'
+                else:
+                    stops = inbound_stops
+                    direction = 'Inbound'
+                for stop in trip['stop_times']:
+                    if StopPoint.objects.filter(atco_code=stop['stop_id']).exists():
+                        stops[stop['stop_id']] = StopUsage(
+                            service=service,
+                            stop_id=stop['stop_id'],
+                            order=stop['stop_sequence'],
+                            direction=direction
+                        )
+                    else:
+                        print(stop['stop_id'])
+            StopUsage.objects.bulk_create(outbound_stops.values())
+            StopUsage.objects.bulk_create(inbound_stops.values())
+
+            service.region = Region.objects.filter(adminarea__stoppoint__service=service).annotate(
+                Count('adminarea__stoppoint__service')
+            ).order_by('-adminarea__stoppoint__service__count').first()
+            service.save()
+
+        for operator in operators:
+            print(operator)
+            print(operator.region)
+            operator.region = Region.objects.filter(adminarea__stoppoint__service__operator=operator).annotate(
+                Count('adminarea__stoppoint__service__operator')
+            ).order_by('-adminarea__stoppoint__service__operator__count').first()
+            operator.save()
+
+    @transaction.atomic
     def handle(self, *args, **options):
+        regions = ('LE', 'CO', 'UL', 'MU')
+        Service.objects.filter(region_id__in=regions).delete()
+        Operator.objects.filter(region_id__in=regions).delete()
         session = requests.Session()
 
         for collection in COLLECTIONS:
+            print(collection)
             path = 'google_transit_{}.zip'.format(collection)
             url = 'http://www.transportforireland.ie/transitData/' + path
             if os.path.exists(path):
@@ -97,4 +164,4 @@ class Command(BaseCommand):
                     write_zip_file(path, response)
             else:
                 write_zip_file(path, session.get(url, stream=True))
-            self.handle_zipfile(path)
+            self.handle_zipfile(path, collection)
