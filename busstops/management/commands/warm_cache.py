78c55a755ac66da8eb9a2db3b6c59c1f2663b5dc
Josh Goodwin <j@joshuagoodw.in>
2017-05-27 19:33:46 +0100

    Remove warm_cache

--- busstops/management/commands/warm_cache.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from ...models import Service
-from ...utils import timetable_from_service
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        for service in Service.objects.filter(current=True).exclude(region='NI'):
-            timetable_from_service(service)
