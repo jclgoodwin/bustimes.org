f9a0b1b434de46fc114ebe4257b38a84b4c230d6
Josh Goodwin <j@joshuagoodw.in>
2017-06-22 00:17:12 +0100

    Use pygtfs for Ireland import (much improved)

--- busstops/management/commands/import_ie_gtfs.py
+++ busstops/management/commands/import_ie_gtfs.py
@@ -1,27 +1,22 @@
 import os
 import time
-import zipfile
-import csv
+import pygtfs
 import requests
 from email.utils import parsedate
 from django.contrib.gis.geos import Point
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.db.models import Count
-from txc.ie import COLLECTIONS
+from txc.ie import COLLECTIONS, get_schedule, get_feed, get_timetable
 from ...models import Operator, Service, StopPoint, StopUsage, Region
 
 
 MODES = {
-    '2': 'rail',
-    '3': 'bus',
-    '4': 'ferry'
+    2: 'rail',
+    3: 'bus',
+    4: 'ferry'
 }
 SESSION = requests.Session()
-
-
-def get_rows(csv_file):
-    return csv.DictReader(line.decode('utf-8-sig') for line in csv_file)
 
 
 def write_zip_file(path, response):
@@ -49,97 +44,78 @@ class Command(BaseCommand):
     @transaction.atomic
     def handle_zipfile(self, archive_name, collection):
         collection = collection[:10]
-        Service.objects.filter(service_code__startswith=collection).update(current=False)
+        Service.objects.filter(service_code__startswith=collection).delete()
+
+        # agencies = {}
+        operators = set()
+
+        schedule = get_schedule()
+        pygtfs.append_feed(schedule, archive_name)  # this could take a while :(
+
+        feed = get_feed(schedule, archive_name)
+        for stop in feed.stops:
+            if stop.id[0] in '78' and not StopPoint.objects.filter(atco_code=stop.id).exists():
+                StopPoint.objects.create(
+                    atco_code=stop.id,
+                    latlong=Point(float(stop.stop_lon), float(stop.stop_lat)),
+                    common_name=stop.stop_name[:48],
+                    locality_centre=False,
+                    admin_area_id=stop.id[:3],
+                    active=True
+                )
 
         routes = {}
-        trips = {}
-        agencies = {}
-        operators = set()
-        with zipfile.ZipFile(archive_name) as archive:
-            with archive.open('stops.txt') as csv_file:
-                for row in get_rows(csv_file):
-                    if row['stop_id'][0] in '78' and not StopPoint.objects.filter(atco_code=row['stop_id']).exists():
-                        StopPoint.objects.create(
-                            atco_code=row['stop_id'],
-                            latlong=Point(float(row['stop_lon']), float(row['stop_lat'])),
-                            common_name=row['stop_name'][:48],
-                            locality_centre=False,
-                            admin_area_id=row['stop_id'][:3],
-                            active=True
-                        )
-            with archive.open('agency.txt') as csv_file:
-                for row in get_rows(csv_file):
-                    agencies[row['agency_id']] = row
-            with archive.open('routes.txt') as csv_file:
-                for row in get_rows(csv_file):
-                    routes[row['route_id']] = row
-                    routes[row['route_id']]['trips'] = []
-            with archive.open('trips.txt') as csv_file:
-                for row in get_rows(csv_file):
-                    trips[row['trip_id']] = row
-                    trips[row['trip_id']]['stop_times'] = []
-                    routes[row['route_id']]['trips'].append(row)
-            with archive.open('stop_times.txt') as csv_file:
-                for row in get_rows(csv_file):
-                    trips[row['trip_id']]['stop_times'].append(row)
+        for route in feed.routes:
+            route_id = '-'.join(route.id.split('-')[-1])
+            routes[route_id] = route
 
-        for route in routes.values():
+        for route_id in routes:
+            service_code = collection + '-' + route_id
+            timetable = get_timetable(service_code, None)[0]
+
+            route = routes[route_id]
+
             defaults = {
                 'region_id': 'LE',
-                'line_name': route['route_short_name'],
-                'description': route['route_long_name'],
+                'line_name': route.route_short_name,
+                'description': route.route_long_name or timetable.groupings[0].name,
                 'date': time.strftime('%Y-%m-%d'),
-                'mode': MODES.get(route['route_type'], ''),
+                'mode': MODES.get(route.route_type, ''),
                 'current': True
             }
-            if route['trips']:
-                for trip in route['trips']:
-                    if not defaults['description']:
-                        defaults['description'] = trip['trip_headsign'].strip()
-                    if trip['direction_id'] == '0' and not defaults.get('outbound_description'):
-                        defaults['outbound_description'] = trip['trip_headsign'].strip()
-                    elif trip['direction_id'] == '1' and not defaults.get('inbound_description'):
-                        defaults['inbound_description'] = trip['trip_headsign'].strip()
-            else:
-                break
-
-            route_id = route['route_id'].split()[0]
             service, created = Service.objects.update_or_create(
-                service_code='{}-{}'.format(collection, '-'.join(route_id.split('-')[:-1])),
+                service_code=service_code,
                 defaults=defaults
             )
-            # if not created:
-            #     print(service)
-            if route['agency_id']:
-                operator = Operator.objects.get_or_create(name=agencies[route['agency_id']]['agency_name'], defaults={
-                    'id': route['agency_id'],
-                    'region_id': 'LE',
-                    'vehicle_mode': defaults['mode']
-                })[0]
-                service.operator.add(operator)
-                operators.add(operator)
 
-            outbound_stops = {}
-            inbound_stops = {}
-            for trip in route['trips']:
-                if trip['direction_id'] == '0':
-                    stops = outbound_stops
-                    direction = 'Outbound'
-                else:
-                    stops = inbound_stops
-                    direction = 'Inbound'
-                for stop in trip['stop_times']:
-                    if StopPoint.objects.filter(atco_code=stop['stop_id']).exists():
-                        stops[stop['stop_id']] = StopUsage(
-                            service=service,
-                            stop_id=stop['stop_id'],
-                            order=stop['stop_sequence'],
-                            direction=direction
+            if not created:
+                print(service)
+
+            operator = Operator.objects.get_or_create(name=route.agency.agency_name, defaults={
+                'id': route.agency_id,
+                'region_id': 'LE',
+                'vehicle_mode': defaults['mode']
+            })[0]
+            service.operator.add(operator)
+            operators.add(operator)
+
+            direction = 'Outbound'
+            stops = []
+            for grouping in timetable.groupings:
+                for i, row in enumerate(grouping.rows):
+                    stop_id = row.part.stop.atco_code
+                    if StopPoint.objects.filter(atco_code=stop_id).exists():
+                        stops.append(
+                            StopUsage(
+                                service=service,
+                                stop_id=stop_id,
+                                order=i,
+                                direction=direction
+                            )
                         )
                     else:
-                        print(stop['stop_id'])
-            StopUsage.objects.bulk_create(outbound_stops.values())
-            StopUsage.objects.bulk_create(inbound_stops.values())
+                        print(stop_id)
+            StopUsage.objects.bulk_create(stops)
 
             service.region = Region.objects.filter(adminarea__stoppoint__service=service).annotate(
                 Count('adminarea__stoppoint__service')
--- txc/ie.py
+++ txc/ie.py
@@ -19,10 +19,6 @@ COLLECTIONS = (
 )
 
 
-def get_rows(csv_file):
-    return csv.DictReader(line.decode('utf-8-sig') for line in csv_file)
-
-
 def handle_trips(trips, day):
     i = 0
     head = None
@@ -81,6 +77,16 @@ def handle_trips(trips, day):
     return g
 
 
+def get_schedule():
+    return pygtfs.Schedule(os.path.join(settings.DATA_DIR, 'gtfs.sqlite'))
+
+
+def get_feed(schedule, name):
+    for feed in schedule.feeds:
+        if name.endswith(feed.feed_name):
+            return feed
+
+
 def get_timetable(service_code, day):
     parts = service_code.split('-', 1)
     path = parts[0]
@@ -92,36 +98,30 @@ def get_timetable(service_code, day):
 
     path = 'google_transit_' + collection + '.zip'
 
-    schedule = pygtfs.Schedule(os.path.join(settings.DATA_DIR, 'gtfs.sqlite'))
-    feed = None
-    for possible_feed in schedule.feeds:
-        if path == possible_feed.feed_name:
-            feed = possible_feed
-            break
+    schedule = get_schedule()
+    feed = get_feed(schedule, path)
     if not feed:
         pygtfs.append_feed(schedule, os.path.join(settings.DATA_DIR, path))  # this could take a while :(
-    for possible_feed in schedule.feeds:
-        if path == possible_feed.feed_name:
-            feed = possible_feed
-            break
+    feed = get_feed(schedule, path)
 
     trips = {}
     routes = (route for route in feed.routes if route.id.startswith(route_id))
     for route in routes:
         for trip in route.trips:
-            service = trip.service
-            exception_type = None
-            for exception in feed.service_exceptions:
-                if exception.service_id == service.id and exception.date == day:
-                    exception_type = exception.exception_type
-                    break
-            if exception_type == 2:  # service has been removed for the specified date
-                continue
-            elif exception_type is None:
-                if day < service.start_date or day > service.end_date:
-                    continue  # outside of dates
-                if not getattr(service, day.strftime('%A').lower()):
+            if day:
+                service = trip.service
+                exception_type = None
+                for exception in feed.service_exceptions:
+                    if exception.service_id == service.id and exception.date == day:
+                        exception_type = exception.exception_type
+                        break
+                if exception_type == 2:  # service has been removed for the specified date
                     continue
+                elif exception_type is None:
+                    if day < service.start_date or day > service.end_date:
+                        continue  # outside of dates
+                    if not getattr(service, day.strftime('%A').lower()):
+                        continue
             if trip.direction_id in trips:
                 trips[trip.direction_id].append(trip)
             else:
