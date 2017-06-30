82c6a0859f14f6b0c662d869bddc68b278f2afd0
Josh Goodwin <j@joshuagoodw.in>
2017-06-30 22:35:42 +0100

    Fix

--- busstops/static/js/global.js
+++ busstops/static/js/global.js
@@ -17,15 +17,14 @@ if (navigator.serviceWorker && location.host !== 'localhost:8000') {
     'use strict';
 
     var ad = document.getElementById('ad'),
-        script,
-        random;
+        script;
 
     if (!ad.innerHTML.trim()) {
-        script = document.createElement('script');
 
-        if (ad.clientWidth >= 768 && Math.random() > 0.3) {
-            postscribe('https://ap.lijit.com/www/delivery/fpi.js?z=440001&width=728&height=90');
+        if (ad.clientWidth >= 768 && Math.random() > 0.1) {
+            window.postscribe(ad, '<script src="https://ap.lijit.com/www/delivery/fpi.js?z=440001&width=728&height=90"></script>');
         } else {
+            script = document.createElement('script');
             (window.adsbygoogle = window.adsbygoogle || []).push({});
             script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
             ad.innerHTML = '<ins class="adsbygoogle" data-ad-client="ca-pub-4420219114164200" data-ad-slot="5070920457" data-ad-format="auto"></ins>';
