3db5a9998fa900ce148f043a19696eb6f5e4b28c
Josh Goodwin <j@joshuagoodw.in>
2017-08-20 19:25:49 +0100

    Fix flake8

--- busstops/management/tests/test_import_maneo.py
+++ busstops/management/tests/test_import_maneo.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from ...models import StopPoint
 from ..commands import import_maneo_stops
 
+
 class ImportManeoTest(TestCase):
     @classmethod
     def setUpTestData(cls):
