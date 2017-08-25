00d55268167975849a1ff0572bc2a2591f63f701
Josh Goodwin <j@joshuagoodw.in>
2017-08-25 21:14:45 +0100

    Quick, add some bank holiday dates

--- txc/txc.py
+++ txc/txc.py
@@ -26,9 +26,11 @@ BANK_HOLIDAYS = {
     datetime.date(2017, 4, 17): ('EasterMonday', 'HolidayMondays'),
     datetime.date(2017, 5, 1): ('MayDay', 'HolidayMondays'),
     datetime.date(2017, 5, 29): ('SpringBank', 'HolidayMondays'),
+    datetime.date(2017, 5, 29): ('LateSummerBankHolidayNotScotland', 'HolidayMondays'),
     datetime.date(2017, 12, 24): ('ChristmasEve',),
-    datetime.date(2017, 12, 25): ('ChristmasDay',),
+    datetime.date(2017, 12, 25): ('ChristmasDay', 'ChristmasDayHoliday'),
     datetime.date(2017, 12, 26): ('BoxingDay',),
+    datetime.date(2018, 1, 1): ('NewYearsDay' 'NewYearsDayHoliday',),
 }
 
 
