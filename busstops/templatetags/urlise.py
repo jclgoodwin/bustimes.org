1ee311bd713914ebfe00cdf38159319f002cbd3a
Josh Goodwin <j@joshuagoodw.in>
2017-08-26 20:00:05 +0100

    Fix Megabus operator page URLs

--- busstops/templatetags/urlise.py
+++ busstops/templatetags/urlise.py
@@ -26,9 +26,7 @@ def urlise(value, autoescape=None):
             'https%3A%2F%2Fuk.megabus.com'
         )
         markup = markup.replace('"http://megabus.com"', megabus, 1)
-        markup = markup.replace('"http://uk.megabus.com"', megabus, 1)
-        markup = markup.replace('"http://www.megabus.com"', megabus, 1)
-        markup = markup.replace('"http://megabusgold.com/"', megabus, 1)
+        markup = markup.replace('"https://uk.megabus.com"', megabus, 1)
     elif 'nationalexpress' in markup:
         national_express = '"{}"'.format(viglink('http://www.nationalexpress.com'))
         markup = markup.replace('"http://nationalexpress.com"', national_express, 1)
