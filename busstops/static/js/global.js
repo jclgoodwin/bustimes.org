2edafdb82a7be46ae0b907391b05632d3409f367
Josh Goodwin <j@joshuagoodw.in>
2017-09-22 19:59:26 +0100

    Fix ESLint

--- busstops/static/js/global.js
+++ busstops/static/js/global.js
@@ -9,10 +9,10 @@
     if (ad && !ad.innerHTML.trim()) {
 
         if (ad.clientWidth >= 768) {
-            window.medianet_width = "728";
-            window.medianet_height = "90";
-            window.medianet_crid = "554393111";
-            window.medianet_versionId = "3111299";
+            window.medianet_width = '728';
+            window.medianet_height = '90';
+            window.medianet_crid = '554393111';
+            window.medianet_versionId = '3111299';
             window.postscribe(ad, '<script src="//contextual.media.net/nmedianet.js?cid=8CUDT4GNF"></script>');
         } else {
             script = document.createElement('script');
