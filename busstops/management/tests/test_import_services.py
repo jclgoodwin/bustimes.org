e3b91d398e9d37e9a08210afed0e7a6514d9e4d0
Josh Goodwin <j@joshuagoodw.in>
2017-12-27 15:02:05 +0000

    Speed up test_import_services (21 seconds -> 11 seconds!?) by using the cache

--- busstops/management/tests/test_import_services.py
+++ busstops/management/tests/test_import_services.py
@@ -279,6 +279,7 @@ class ImportServicesTest(TestCase):
         self.assertEqual(1, service.stopusage_set.all().count())
 
     @freeze_time('1 October 2017')
+    @override_settings(CACHES={'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}})
     def test_service_dates(self):
         self.assertEqual(0, ServiceDate.objects.count())
 
