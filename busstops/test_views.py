9b838dd792a4b1578f240ffbf62d27d922ab198f
Josh Goodwin <j@joshuagoodw.in>
2018-01-01 23:08:20 +0000

    Fix regions view test

--- busstops/test_views.py
+++ busstops/test_views.py
@@ -153,7 +153,6 @@ class ViewsTests(TestCase):
     def test_index(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['regions'][0], self.north)
 
     def test_offline(self):
         response = self.client.get('/offline')
