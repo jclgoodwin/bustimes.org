47738ce24e886ac4bfd3ddeb035691ca2e4a2c53
Josh Goodwin <j@joshuagoodw.in>
2018-04-06 19:32:46 +0100

    Stop AdSense auto ads again, update Raven.js

--- busstops/static/js/global.js
+++ busstops/static/js/global.js
@@ -10,18 +10,3 @@ if (navigator.serviceWorker && location.host !== 'localhost:8000') {
         }
     });
 }
-
-if (window.adsbygoogle) {
-    var clientWidth = document.documentElement.clientWidth;
-    if (clientWidth >= 1080 || (clientWidth < 768 && window.location.href.indexOf('/services/') === -1)) {
-        var s = document.createElement('script');
-        s.async = true;
-        s.src = 'https://sac.ayads.co/sublime/21256';
-        document.documentElement.appendChild(s);
-    } else {
-        window.adsbygoogle.push({
-            google_ad_client: 'ca-pub-4420219114164200',
-            enable_page_level_ads: true
-        });
-    }
-}
