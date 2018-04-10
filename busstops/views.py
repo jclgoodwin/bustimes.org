5beb9eb574ef70930278385f382614c71374c729
Josh Goodwin <j@joshuagoodw.in>
2018-04-10 21:31:22 +0100

    B-155

--- busstops/views.py
+++ busstops/views.py
@@ -129,6 +129,8 @@ def contact(request):
 @csrf_exempt
 def awin_transaction(request):
     json_string = request.POST.get('AwinTransactionPush') or request.body
+    if not json_string
+        return HttpResponseBadRequest()
     data = json.loads(json_string)
     message = '\n'.join('%s: %s' % pair for pair in data.items())
     EmailMessage(
