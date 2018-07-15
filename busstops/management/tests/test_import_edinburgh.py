6e87a32eb62c8dd0a784814a27a100f239cade3c
Josh Goodwin <j@joshuagoodw.in>
2018-07-15 14:57:17 +0100

    Test Reading and Edinburgh imports

---
+++ busstops/management/tests/test_import_edinburgh.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from ...models import DataSource, Service, Vehicle, Operator, Region
+from ..commands.import_edinburgh import Command
+
+
+
+class EdinburghImportTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        source = DataSource.objects.create(name='TfE', url='', datetime='1066-01-01 12:18Z')
+        Region.objects.create(name='Scotch Land', id='S')
+        cls.operator = Operator.objects.create(name="Ainsley's Chariots", id='AINS', region_id='S')
+        cls.service = Service.objects.create(line_name='corbyn', date='1904-05-05')
+        cls.service.operator.add(cls.operator)
+        cls.vehicle = Vehicle(source=source)
+        cls.command = Command()
+        cls.command.source = source
+
+    def test_get_vehicle_and_service(self):
+        item = {
+            'vehicle_id': 'jeremy',
+            'service_name': 'corbyn'
+        }
+        vehicle, created, service = self.command.get_vehicle_and_service(item)
+        self.assertIsNone(vehicle.operator)
+        self.assertTrue(created)
+        self.assertIsNone(service)
+
+    def test_vehicle_location(self):
+        item = {
+            'vehicle_id': 'jeremy',
+            'latitude': 55.95376,
+            'longitude': -3.18718,
+            'heading': 76,
+            'service_name': 'corbyn'
+        }
+        location = self.command.create_vehicle_location(item, self.vehicle, self.service)
+        self.assertTrue(location.latlong)
+        self.assertEqual(self.vehicle.operator, self.operator)
---
+++ busstops/management/tests/test_import_reading.py
@@ -0,0 +1,66 @@
+from django.test import TestCase
+from django.db.utils import IntegrityError
+from ...models import DataSource, Service, Vehicle, Operator, Region
+from ..commands.import_reading import Command
+
+
+
+class ReadingImportTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        source = DataSource.objects.create(name='Reading', url='', datetime='1066-01-01 12:18Z')
+        Region.objects.create(name='Scotch Land', id='S')
+
+        cls.otis = Operator.objects.create(name="Otis Redding's Buses", id='RBUS', region_id='S')
+        cls.kenn = Operator.objects.create(name="Kenny's Coaches", id='KENN', region_id='S')
+        cls.thames = Operator.objects.create(name='Thames', id='THVB', region_id='S')
+        cls.green = Operator.objects.create(name='Green Line', id='GLRB', region_id='S')
+
+        cls.service = Service.objects.create(line_name='102', date='1904-05-05')
+        cls.service.operator.add(cls.otis)
+
+        cls.vehicle = Vehicle(source=source)
+        cls.command = Command()
+        cls.command.source = source
+
+        cls.item = {
+            'vehicle': '751',
+            'service': 'k102',
+            'observed': '2018-07-13 12:40:07',
+            'latitude': '51.443426',
+            'longitude': '-0.955788',
+            'bearing': '301'
+        }
+
+    def test_get_vehicle_and_service(self):
+        vehicle, created, service = self.command.get_vehicle_and_service(self.item)
+        self.assertEqual(self.otis, vehicle.operator)
+        self.assertTrue(created)
+        self.assertEqual(self.service, service)
+
+        self.item['service'] = 'TV702'
+        vehicle, _, service = self.command.get_vehicle_and_service(self.item)
+        self.assertEqual(self.thames, vehicle.operator)
+        self.assertIsNone(service)
+
+        self.item['service'] = 'K702'
+        vehicle, _, _ = self.command.get_vehicle_and_service(self.item)
+        self.assertEqual(self.kenn, vehicle.operator)
+
+        self.item['service'] = ''
+        vehicle, _, service = self.command.get_vehicle_and_service(self.item)
+        self.assertEqual(self.otis, vehicle.operator)
+        self.assertIsNone(service)
+
+        self.item['service'] = '702'
+        vehicle, _, _ = self.command.get_vehicle_and_service(self.item)
+        self.assertEqual(self.green, vehicle.operator)
+
+    def test_vehicle_location(self):
+        location = self.command.create_vehicle_location(self.item, self.vehicle, self.service)
+        self.assertTrue(location.latlong)
+        self.assertTrue(location.heading)
+        self.assertIsNone(location.early)
+
+        location.data = self.item
+        self.assertEqual('702', location.get_label())
