0d13507155b9ea1f97851db40fe9eb4de70ab842
Josh Goodwin <j@joshuagoodw.in>
2018-08-07 17:25:28 +0200

    unsatisfactorily fix B-18K

--- busstops/management/commands/correct_stops.py
+++ busstops/management/commands/correct_stops.py
@@ -2,6 +2,7 @@ import yaml
 import os
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.contrib.gis.geos import GEOSGeometry
 from ...models import StopPoint
 
 
@@ -12,5 +13,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open(os.path.join(settings.DATA_DIR, 'stops.yaml')) as open_file:
             records = yaml.load(open_file)
-            for atco_code in records:
-                StopPoint.objects.filter(atco_code=atco_code).update(**records[atco_code])
+            for atco_code, record in records.items():
+                if 'latlong' in record:
+                    record['latlong'] = GEOSGeometry(record['latlong'])
+                StopPoint.objects.filter(atco_code=atco_code).update(**record)
