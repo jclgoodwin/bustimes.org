351c7645c43e217d9173362f0939648fd2c6123f
Josh Goodwin <j@joshuagoodw.in>
2018-08-07 14:41:53 +0200

    Fix Siri VM test

--- busstops/management/tests/test_import_sirivm.py
+++ busstops/management/tests/test_import_sirivm.py
@@ -1,14 +1,12 @@
 import os
 from vcr import use_cassette
-# from mock import patch
 from django.test import TestCase
 from ...models import DataSource
-# with patch('time.sleep', return_value=None):
 from ..commands import import_sirivm
 
 
-@use_cassette(os.path.join('data', 'vcr', 'import_sirivm.yaml'), decode_compressed_response=True)
 class SiriVMImportTest(TestCase):
+    @use_cassette(os.path.join('data', 'vcr', 'import_sirivm.yaml'), decode_compressed_response=True)
     def test_handle(self):
         command = import_sirivm.Command()
         items = command.get_items()
@@ -18,6 +16,6 @@ class SiriVMImportTest(TestCase):
 
         vehicle, created, service = command.get_vehicle_and_service(item)
 
-        self.assertEqual('FE 67162', str(vehicle))
+        self.assertEqual('FE 69532', str(vehicle))
         self.assertTrue(created)
         self.assertIsNone(service)
