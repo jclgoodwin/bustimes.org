6975ce27dc91645525dd321ae4e0361d7653c031
Josh Goodwin <j@joshuagoodw.in>
2018-09-27 17:57:10 +0100

    Show latest location on operator vehicles list

--- busstops/views.py
+++ busstops/views.py
@@ -540,7 +540,8 @@ def operator_vehicles(request, slug):
     return render(request, 'operator_vehicles.html', {
         'breadcrumb': [operator.region, operator],
         'object': operator,
-        'vehicles': operator.vehicle_set.order_by('fleet_number').select_related('vehicle_type')
+        'vehicles': operator.vehicle_set.order_by('fleet_number').select_related('vehicle_type',
+                                                                                 'latest_location__service')
     })
 
 
