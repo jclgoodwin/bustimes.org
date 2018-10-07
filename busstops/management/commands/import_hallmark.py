f0bf62bf744073712424f063807eaca34faafe8b
Josh Goodwin <j@joshuagoodw.in>
2018-10-07 22:43:00 +0100

    h

---
+++ busstops/management/commands/import_hallmark.py
@@ -0,0 +1,10 @@
+from .import_bushub import Command as BusHubCommand
+
+
+class Command(BusHubCommand):
+    source_name = 'Hallmark'
+    url = 'http://portal.hallmarkbus.com/api/buses/nearby?latitude&longitude'
+
+    def get_vehicle_and_service(self, item):
+        item['OperatorRef'] = 'WNGS'
+        return super().get_vehicle_and_service(item)
---
+++ busstops/management/commands/import_hotel_hoppa.py
@@ -0,0 +1,10 @@
+from .import_bushub import Command as BusHubCommand
+
+
+class Command(BusHubCommand):
+    source_name = 'Hotel Hoppa'
+    url = 'http://portal.hotelhoppa.co.uk/api/buses/nearby?latitude&longitude'
+
+    def get_vehicle_and_service(self, item):
+        item['OperatorRef'] = 'NXHH'
+        return super().get_vehicle_and_service(item)
