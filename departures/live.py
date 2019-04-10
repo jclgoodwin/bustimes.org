b94c7ec3c5ba17841d10ff9e85630cb34a8e6fb9
Josh Goodwin <j@joshuagoodw.in>
2019-04-10 13:41:16 +0100

    Try using Transport API for First Scotland East and West Yorkshire

--- departures/live.py
+++ departures/live.py
@@ -734,6 +734,8 @@ def get_departures(stop, services, bot=False):
 
             if source:
                 live_rows = SiriSmDepartures(source, stop, services).get_departures()
+            elif stop.atco_code[:3] == '450' or any(operator.id in {'FSCE'} for operator in operators):
+                live_rows = TransportApiDepartures(stop, services, now.date()).get_departures()
             elif stop.atco_code[:3] == '430':
                 live_rows = WestMidlandsDepartures(stop, services).get_departures()
             elif any(operator.id in {'YCST', 'HRGT', 'KDTR'} for operator in operators):
