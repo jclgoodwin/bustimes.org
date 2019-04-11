ca287bf5f22637d98af2ea9c78b499c7fc058766
Josh Goodwin <j@joshuagoodw.in>
2019-04-11 18:11:18 +0100

    Revert "catch service worker error?"
    This reverts commit c14cfe8917c1f1c23ccd4bbd25a7b40d6c5d6b2b.

--- busstops/static/js/global.js
+++ busstops/static/js/global.js
@@ -1,16 +1,14 @@
 /*jslint browser: true*/
 
-if (navigator.serviceWorker && location.protocol === 'https://') {
-    try {
-        navigator.serviceWorker.register('/serviceworker.js', {
-            scope: '/'
-        });
-        window.addEventListener('load', function () {
-            if (navigator.serviceWorker.controller) {
-                navigator.serviceWorker.controller.postMessage({'command': 'trimCaches'});
-            }
-        });
-    } catch {}
+if (navigator.serviceWorker && location.host !== 'localhost:8000') {
+    navigator.serviceWorker.register('/serviceworker.js', {
+        scope: '/'
+    });
+    window.addEventListener('load', function () {
+        if (navigator.serviceWorker.controller) {
+            navigator.serviceWorker.controller.postMessage({'command': 'trimCaches'});
+        }
+    });
 }
 
 (function () {
