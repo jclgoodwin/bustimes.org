9e77d2fc5c5fddd0338ce3e1b91b8aa89fe0b458
Josh Goodwin <j@joshuagoodw.in>
2019-05-04 19:32:28 +0100

    big change

--- vehicles/management/commands/import_big_change.py
+++ vehicles/management/commands/import_big_change.py
@@ -3,7 +3,7 @@ from ciso8601 import parse_datetime
 from django.db.transaction import atomic
 from django.contrib.gis.geos import Point
 from django.core.management.base import BaseCommand
-from busstops.models import DataSource
+from busstops.models import DataSource, Service
 from ...models import Vehicle, VehicleJourney, VehicleLocation
 
 
@@ -18,30 +18,55 @@ def on_connect():
 
 
 @sio.on('posUpdate')
-@atomic
 def on_message(data):
     print(data)
     vehicle, created = Vehicle.objects.get_or_create(operator_id='MSOT', code=data['ass'])
     datetime = parse_datetime(data['date'])
-    service = data['extra'].get('custR', '')
-    if created or not vehicle.latest_location or vehicle.latest_location.journey.route_name != service:
+    latlong = Point(data['lng'], data['lat'])
+
+    journey = None
+    service = None
+
+    if not created:
+        latest_location = vehicle.latest_location
+        current = latest_location and (datetime - latest_location.datetime).total_seconds() < 180
+        if current and latest_location.journey.service and latest_location.journey.service.geometry.overlaps(latlong):
+            journey = latest_location.journey
+
+        if not journey:
+            try:
+                service = Service.objects.filter(
+                    operator='MSOT',
+                    journey__datetime__lte=datetime,
+                    journey__stopusageusage__datetime__gte=datetime,
+                    geometry__bboverlaps=latlong
+                ).distinct().get()
+            except (Service.DoesNotExist, Service.MultipleObjectsReturned):
+                pass
+
+        if current and not service and not latest_location.journey.service:
+            journey = latest_location.journey
+
+    if not journey:
         journey = VehicleJourney.objects.create(
             vehicle=vehicle,
             datetime=datetime,
             source=globalism['source'],
-            route_name=service
-        ).id
-    else:
-        vehicle.latest_location.journey.vehiclelocation_set.update(current=False)
-        journey = vehicle.latest_location.journey_id
-    vehicle.latest_location = VehicleLocation.objects.create(
-        journey_id=journey,
-        datetime=datetime,
-        latlong=Point(data['lng'], data['lat']),
-        heading=data['dir'],
-        current=True
-    )
-    vehicle.save()
+            service=service,
+            route_name=data['extra'].get('custR', '')
+        )
+
+    with atomic():
+        if not created and latest_location:
+            latest_location.journey.vehiclelocation_set.update(current=False)
+        vehicle.latest_location = VehicleLocation.objects.create(
+            journey=journey,
+            datetime=datetime,
+            latlong=latlong,
+            heading=data['dir'],
+            current=True
+        )
+        vehicle.save()
 
 
 @sio.on('disconnect')
