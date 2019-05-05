c7cb021ff27e6ec2c4b7e3e264e3616b03212e47
Josh Goodwin <j@joshuagoodw.in>
2019-05-05 12:20:44 +0100

    fix hoppa, hallmark

--- vehicles/management/commands/import_hallmark.py
+++ vehicles/management/commands/import_hallmark.py
@@ -5,6 +5,6 @@ class Command(BusHubCommand):
     source_name = 'Hallmark'
     url = 'http://portal.hallmarkbus.com/api/buses/nearby?latitude&longitude'
 
-    def get_journey(self, item):
+    def get_vehicle(self, item):
         item['OperatorRef'] = 'WNGS'
         return super().get_journey(item)
--- vehicles/management/commands/import_hotel_hoppa.py
+++ vehicles/management/commands/import_hotel_hoppa.py
@@ -5,6 +5,6 @@ class Command(BusHubCommand):
     source_name = 'Hotel Hoppa'
     url = 'http://portal.hotelhoppa.co.uk/api/buses/nearby?latitude&longitude'
 
-    def get_journey(self, item):
+    def get_vehicle(self, item):
         item['OperatorRef'] = 'NXHH'
         return super().get_journey(item)
