77fca3a750d1053be3fc70d2232681392681510e
Josh Goodwin <j@joshuagoodw.in>
2019-06-04 13:05:09 +0100

    Start to detect fast-moving (in two places at same time?) vehicles

--- vehicles/management/import_live_vehicles.py
+++ vehicles/management/import_live_vehicles.py
@@ -168,6 +168,12 @@ class ImportLiveVehiclesCommand(BaseCommand):
             latest.current = False
             latest.save()
 
+            distance = latest.latlong.distance(location.latlong) * 69
+            time = location.datetime - latest.datetime
+            speed = distance / time.total_seconds() * 60 * 60
+            if speed > 55:
+                print('{} mph\t{}'.format(speed, journey.vehicle.get_absolute_url()))
+
     def update(self):
         now = timezone.now()
         self.source, source_created = DataSource.objects.update_or_create(
