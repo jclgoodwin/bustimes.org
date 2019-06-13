37aa81198018540dd2c10a1894aaf369360943f3
Josh Goodwin <j@joshuagoodw.in>
2019-06-13 11:04:58 +0100

    Fix - only correct operator for Cumfy/Tyrers and Gall/Steph/NIBS

--- vehicles/management/commands/import_live_ziptrip.py
+++ vehicles/management/commands/import_live_ziptrip.py
@@ -184,16 +184,14 @@ class Command(ImportLiveVehiclesCommand):
                 services = services.filter(operator=operator_id)
             try:
                 journey.service = self.get_service(services, get_latlong(item))
-                try:
-                    operator = journey.service.operator.get()
-                    if vehicle.operator_id != operator.id:
-                        vehicle.operator = operator
-                        vehicle.save()
-                except Operator.MultipleObjectsReturned:
-                    pass
-                if operator_id[0] == 'SESX' and journey.service.operator.first().id != vehicle.operator_id:
-                    vehicle.operator = journey.service.operator.first()
-                    vehicle.save()
+                if operator_id[0] == 'SESX' or operator_id[0] == 'CUBU':
+                    try:
+                        operator = journey.service.operator.get()
+                        if vehicle.operator_id != operator.id:
+                            vehicle.operator = operator
+                            vehicle.save()
+                    except Operator.MultipleObjectsReturned:
+                        pass
             except (Service.MultipleObjectsReturned, Service.DoesNotExist) as e:
                 if route_name.lower() not in self.ignorable_route_names:
                     if operator_id[0] != 'bus-vannin' and not (operator_id[0] == 'RBUS' and route_name[0] == 'V'):
