49f43247943696c33c200f6dca14f84d3958666b
Josh Goodwin <j@joshuagoodw.in>
2019-07-20 19:37:02 +0100

    https://adactio.com/journal/15454

--- busstops/static/serviceworker.js
+++ busstops/static/serviceworker.js
@@ -27,7 +27,7 @@ function trimCache(cacheName, maxItems) {
     caches.open(cacheName).then(cache => {
         cache.keys().then(keys => {
             if (keys.length > maxItems) {
-                cache.delete(keys[0]).then(trimCache(cacheName, maxItems));
+                cache.delete(keys[0]).then(() => trimCache(cacheName, maxItems));
             }
         });
     });
