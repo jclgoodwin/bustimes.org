69fb4cae9036e333c5260750f4485d2e2bc50f7c
Josh Goodwin <j@joshuagoodw.in>
2019-07-25 13:01:51 +0100

    only show operator vehicles notes column if there are notes

--- vehicles/views.py
+++ vehicles/views.py
@@ -55,7 +55,8 @@ def operator_vehicles(request, slug):
         'object': operator,
         'today': timezone.localtime().date(),
         'vehicles': vehicles,
-        'code_column': any(v.code.isdigit() and v.fleet_number and int(v.code) != v.fleet_number for v in vehicles)
+        'code_column': any(v.code.isdigit() and v.fleet_number and int(v.code) != v.fleet_number for v in vehicles),
+        'notes_column': any(vehicle.notes for vehicle in vehicles),
     })
 
 
