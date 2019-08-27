938139899c004fd4cf66e1437e1ec38333a5d531
Josh Goodwin <j@joshuagoodw.in>
2019-08-27 20:12:28 +0000

    cope with different Translink file name

--- data/import.sh
+++ data/import.sh
@@ -70,13 +70,13 @@ fi
 cd ..
 
 # Translink Metro
-metro_old=$(shasum metro--glider.zip)
-wget -qN https://www.opendatani.gov.uk/dataset/6d9677cf-8d03-4851-985c-16f73f7dd5fb/resource/f2c58049-7ca9-4576-b3bd-1b3d8a8674e0/download/metro--glider.zip
-metro_new=$(shasum metro--glider.zip)
+metro_old=$(shasum metro_glider.zip)
+wget -qN https://www.opendatani.gov.uk/dataset/6d9677cf-8d03-4851-985c-16f73f7dd5fb/resource/f2c58049-7ca9-4576-b3bd-1b3d8a8674e0/download/metro_glider.zip
+metro_new=$(shasum metro_glider.zip)
 if [[ "$metro_old" != "$metro_new" ]]; then
     rm -r Metro
     mkdir Metro
-    unzip metro--glider.zip -d Metro
+    unzip metro_glider.zip -d Metro
     echo "Translink Metro"
 fi
 
