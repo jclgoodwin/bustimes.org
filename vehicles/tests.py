40b825cddb76116466457a12430adb2664162af8
Josh Goodwin <j@joshuagoodw.in>
2019-09-25 14:53:53 +0100

    fix test

--- vehicles/tests.py
+++ vehicles/tests.py
@@ -92,7 +92,7 @@ class VehiclesTests(TestCase):
 
         livery.colours = '#7D287D #FDEE00 #FDEE00'
         livery.horizontal = True
-        self.assertEqual('<div style="height:1.5em;width:4em;background:linear-gradient' +
+        self.assertEqual('<div style="height:1.5em;width:2.25em;background:linear-gradient' +
                          '(to top,#7D287D 34%,#FDEE00 34%)" title="Go-Coach"></div>', livery.preview())
         livery.horizontal = False
         livery.angle = 45
