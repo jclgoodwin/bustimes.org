689efb92139b6894b389ac92e7123ecd3ab505f8
Josh Goodwin <j@joshuagoodw.in>
2019-10-02 23:13:15 +0100

    add missing bustimes.urls

---
+++ bustimes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('routes/<int:pk>', views.RouteDetailView.as_view(), name='route_detail'),
+    # path('routes/<int:pk>', views.RouteDetailView.as_view(), name='route_detail'),
+]
