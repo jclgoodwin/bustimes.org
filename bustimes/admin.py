1caef57a400511568aa291b8f668a27cc37bfb59
Josh Goodwin <j@joshuagoodw.in>
2019-10-04 22:08:11 +0100

    filter routes by source

--- bustimes/admin.py
+++ bustimes/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Route, Trip, Calendar
 
 
-admin.site.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'start_date', 'end_date']
+    list_filter = ['source']
+
+
+admin.site.register(Route, RouteAdmin)
 admin.site.register(Trip)
 admin.site.register(Calendar)
