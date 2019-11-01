d1d80b3c4939425018b7970f95817902a10200c0
Josh Goodwin <j@joshuagoodw.in>
2019-11-01 23:17:03 +0000

    journey history map: navigate between, make arrows different, etc

--- busstops/static/js/history.js
+++ busstops/static/js/history.js
@@ -9,13 +9,15 @@
     */
 
     var map,
+        mapContainer = document.getElementById('journey-overlay'),
+        layerGroup = L.layerGroup(),
         tileURL = 'https://maps.bustimes.org/styles/klokantech-basic/{z}/{x}/{y}' + (L.Browser.retina ? '@2x' : '') + '.png';
 
     function getMarker(latlng, direction) {
-        direction = direction || 0;
+        direction = (direction || 0) - 135;
         return L.marker(latlng, {
             icon: L.divIcon({
-                iconSize: [16, 8],
+                iconSize: [10, 10],
                 html: '<div class="arrow" style="-ms-transform: rotate(' + direction + 'deg);-webkit-transform: rotate(' + direction + 'deg);-moz-transform: rotate(' + direction + 'deg);-o-transform: rotate(' + direction + 'deg);transform: rotate(' + direction + 'deg)"></div>',
                 className: 'just-arrow'
             })
@@ -23,8 +25,7 @@
     }
 
     function closeMap() {
-        map.remove();
-        document.body.removeChild(document.getElementById('map'));
+        mapContainer.style.display = 'none';
         return false;
     }
 
@@ -35,9 +36,13 @@
     };
 
     function openMap(event) {
+        var row = event.target.parentElement.parentElement,
+            previous = row.previousElementSibling,
+            next = row.nextElementSibling;
+
         reqwest('/journeys/' + event.target.getAttribute('data-journey-id') + '.json', function(locations) {
-            var mapContainer = document.createElement('div'),
-                i,
+            var i,
+                details = document.getElementById('details'),
                 dateTime,
                 popup,
                 delta,
@@ -45,35 +50,68 @@
                 line = [],
                 bounds;
 
-            mapContainer.id = 'map';
-            mapContainer.className = 'full-screen';
-            document.body.appendChild(mapContainer);
+            mapContainer.style.display = 'block';
 
-            map =  L.map('map');
+            details.innerHTML = '<p>' + row.children[0].innerHTML + ' – ' + row.children[1].innerHTML + ' to ' + row.children[2].innerHTML + '</p>';
 
-            map.attributionControl.setPrefix('');
+            if (previous) {
+                var previousButton = document.createElement('a');
+                previousButton.href = '#';
+                previousButton.innerHTML = '← ' + previous.children[1].innerHTML;
+                previousButton.onclick = function() {
+                    previous.children[3].children[0].click();
+                    return false;
+                };
+                var previousP = document.createElement('p');
+                previousP.className = 'previous';
+                previousP.appendChild(previousButton);
+                details.appendChild(previousP);
+            }
+            if (next) {
+                var nextButton = document.createElement('a');
+                nextButton.href = '#';
+                nextButton.innerHTML = next.children[1].innerHTML + ' →';
+                nextButton.onclick = function() {
+                    next.children[3].children[0].click();
+                    return false;
+                };
+                var nextP = document.createElement('p');
+                nextP.className = 'next';
+                nextP.appendChild(nextButton);
+                details.appendChild(nextP);
+            }
 
-            L.tileLayer(tileURL, {
-                attribution: '<a href="https://www.maptiler.com/copyright/">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>'
-            }).addTo(map);
+            if (map) {
+                layerGroup.clearLayers();
+                map.invalidateSize();
+            } else {
+                map =  L.map('map');
+                layerGroup.addTo(map);
 
-            var closeButton = L.control();
+                map.attributionControl.setPrefix('');
 
-            closeButton.onAdd = function() {
-                var div = document.createElement('div');
-                div.className = 'leaflet-bar';
-                var a = document.createElement('a');
-                a.href = '#';
-                a.style.width = 'auto';
-                a.style.padding = '0 8px';
-                a.setAttribute('role', 'button');
-                a.innerHTML = 'Close map';
-                a.onclick = closeMap;
-                div.appendChild(a);
-                return div;
-            };
+                L.tileLayer(tileURL, {
+                    attribution: '<a href="https://www.maptiler.com/copyright/">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>'
+                }).addTo(map);
 
-            closeButton.addTo(map);
+                var closeButton = L.control();
+
+                closeButton.onAdd = function() {
+                    var div = document.createElement('div');
+                    div.className = 'leaflet-bar';
+                    var a = document.createElement('a');
+                    a.href = '#';
+                    a.style.width = 'auto';
+                    a.style.padding = '0 8px';
+                    a.setAttribute('role', 'button');
+                    a.innerHTML = 'Close map';
+                    a.onclick = closeMap;
+                    div.appendChild(a);
+                    return div;
+                };
+
+                closeButton.addTo(map);
+            }
 
             for (i = locations.length - 1; i >= 0; i -= 1) {
                 dateTime = new Date(locations[i].datetime);
@@ -97,11 +135,11 @@
                     }
                 }
                 coordinates = [locations[i].coordinates[1], locations[i].coordinates[0]];
-                getMarker(coordinates, locations[i].direction).bindPopup(popup).addTo(map);
+                getMarker(coordinates, locations[i].direction).bindPopup(popup).addTo(layerGroup);
                 line.push(coordinates);
             }
             line = L.polyline(line);
-            line.addTo(map);
+            line.addTo(layerGroup);
             bounds = line.getBounds();
             map.fitBounds(bounds);
             map.setMaxBounds(bounds.pad(.5));
