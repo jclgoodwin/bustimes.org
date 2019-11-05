8c668a0886d86c018a523368989193e99cfaa777
Josh Goodwin <j@joshuagoodw.in>
2019-11-05 14:25:11 +0000

    start speed graphs

--- vehicles/views.py
+++ vehicles/views.py
@@ -393,12 +393,18 @@ class JourneyDetailView(DetailView):
 
 
 def journey_json(request, pk):
-    return JsonResponse([{
-        'coordinates': tuple(location.latlong),
-        'delta': location.early,
-        'direction': location.heading,
-        'datetime': location.datetime,
-    } for location in VehicleLocation.objects.filter(journey=pk)], safe=False)
+    previous = None
+    locations = []
+    for location in VehicleLocation.objects.filter(journey=pk):
+        locations.append({
+            'coordinates': tuple(location.latlong),
+            'delta': location.early,
+            'direction': location.heading,
+            'datetime': location.datetime,
+            'speed': previous.calculate_speed(location) if previous else 0
+        })
+        previous = location
+    return JsonResponse(locations, safe=False)
 
 
 def siri(request):
