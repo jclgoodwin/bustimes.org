9f1d52db4db20cd3e677a552fa7f70f84aa4bc5d
Josh Goodwin <j@joshuagoodw.in>
2019-12-21 18:44:16 +0000

    try lazy loading ads

--- busstops/static/js/global.js
+++ busstops/static/js/global.js
@@ -20,4 +20,35 @@ if (navigator.serviceWorker && location.protocol === 'https:') {
         cookieMessage.style.display = 'block';
         document.cookie = 'seen_cookie_message=yes; max-age=31536000; path=/';
     }
+
+    var ads = document.getElementsByClassName('banner-ad');
+    window.adsbygoogle = (window.adsbygoogle || []);
+    window.adsbygoogle.requestNonPersonalizedAds = 1;
+
+    if (window.IntersectionObserver) {
+        var observer = new IntersectionObserver(function(entries) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting && !entry.target.querySelector('.adsbygoogle').innerHTML) {
+                    var script = document.createElement('script');
+                    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+                    window.adsbygoogle.push({});
+                    document.body.appendChild(script);
+                }
+            });
+        });
+    }
+
+    for (var i = ads.length - 1; i >= 0; i -= 1) {
+        if (observer) {
+            observer.observe(ads[i]);
+        } else {
+            window.adsbygoogle.push({});
+        }
+    }
+
+    if (!observer) {
+        var script = document.createElement('script');
+        script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+        document.body.appendChild(script);
+    }
 })();
