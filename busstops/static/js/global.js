d77ad56526d26443ed1906752e494635779258e7
Josh Goodwin <j@joshuagoodw.in>
2020-01-13 13:21:44 +0000

    fix ads

--- busstops/static/js/global.js
+++ busstops/static/js/global.js
@@ -21,14 +21,14 @@ if (navigator.serviceWorker && location.protocol === 'https:') {
         document.cookie = 'seen_cookie_message=yes; max-age=31536000; path=/';
     }
 
-    var ads = document.getElementsByClassName('banner-ad');
+    var ads = document.getElementsByClassName('adsbygoogle');
     if (!ads.length) {
-       return;
+        return;
     }
     window.adsbygoogle = (window.adsbygoogle || []);
     window.adsbygoogle.requestNonPersonalizedAds = 1;
     var script = document.createElement('script');
-    script.dataset['ad-client'] = 'ca-pub-4420219114164200';
+    script.dataset.adClient = 'ca-pub-4420219114164200';
     script.async = 'async';
     script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
     document.body.appendChild(script);
@@ -36,7 +36,7 @@ if (navigator.serviceWorker && location.protocol === 'https:') {
     if (window.IntersectionObserver) {
         var observer = new IntersectionObserver(function(entries) {
             entries.forEach(function(entry) {
-                if (entry.isIntersecting && !entry.target.querySelector('.adsbygoogle').innerHTML) {
+                if (entry.isIntersecting && !entry.target.innerHTML) {
                     window.adsbygoogle.push({});
                 }
             });
