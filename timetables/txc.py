2a2394094c55b3cb92a3ba94eab990959bf7db0a
Josh Goodwin <j@joshuagoodw.in>
2020-04-02 21:17:24 +0100

    nicen some northern descriptions

--- timetables/txc.py
+++ timetables/txc.py
@@ -518,6 +518,10 @@ class Service:
     def set_description(self, description):
         if description.isupper():
             description = titlecase(description)
+        elif ' via ' in description and description[:description.find(' via ')].isupper():
+            parts = description.split(' via ')
+            parts[0] = titlecase(parts[0])
+            description = ' via '.join(parts)
         self.description = correct_description(description)
 
         self.via = None
