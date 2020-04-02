d5ced7d77b08c382b21ae3127fb37502e1468541
Josh Goodwin <j@joshuagoodw.in>
2020-04-03 00:49:58 +0100

    doh

--- bustimes/management/commands/import_transxchange.py
+++ bustimes/management/commands/import_transxchange.py
@@ -444,7 +444,7 @@ class Command(BaseCommand):
                 'show_timetable': True
             }
             description = txc_service.description
-            if description and 'COVID-19 timetable' not in description.lower():
+            if description and 'covid-19 timetable' not in description.lower():
                 if self.region_id == 'NE':
                     description = sanitize_description(description)
                 defaults['description'] = description
