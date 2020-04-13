1de9f9e9584b43bc9bc1c1856e5eefd77e15e7d2
Josh Goodwin <j@joshuagoodw.in>
2020-04-13 13:38:51 +0100

    cope with null existing vehicle data

--- vehicles/models.py
+++ vehicles/models.py
@@ -297,7 +297,7 @@ class VehicleEdit(models.Model):
             changes['withdrawn'] = True
         if self.changes:
             for key in self.changes:
-                if key not in self.vehicle.data or self.changes[key] != self.vehicle.data[key]:
+                if not self.vehicle.data or self.changes[key] != self.vehicle.data.get(key):
                     changes[key] = self.changes[key]
         return changes
 
