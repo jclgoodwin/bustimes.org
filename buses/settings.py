b4644a833b5aa4274fe3f5fe2f6d6c8613e14a76
Josh Goodwin <j@joshuagoodw.in>
2020-06-08 16:33:17 +0000

    flake8

--- buses/settings.py
+++ buses/settings.py
@@ -216,7 +216,7 @@ if not DEBUG and 'test' not in sys.argv and 'collectstatic' not in sys.argv:
         dsn=os.environ.get('SENTRY_DSN'),
         integrations=[DjangoIntegration(), RedisIntegration(), CeleryIntegration()],
         ignore_errors=[KeyboardInterrupt],
-        traces_sample_rate = 0.25
+        traces_sample_rate=0.25
     )
 
 TRANSPORTAPI = {
