ee951e1fbbedaa93db641a36f7c6efe3278a44b8
Josh Goodwin <j@joshuagoodw.in>
2020-07-07 13:37:45 +0100

    start sending some vehicle locations over the websocket

--- vehicles/consumers.py
+++ vehicles/consumers.py
@@ -1,18 +1,29 @@
 import json
+from django.contrib.gis.geos import Polygon
 from channels.generic.websocket import WebsocketConsumer
+# from channels.db import database_sync_to_async
+from .models import VehicleLocation
 
 
 class ChatConsumer(WebsocketConsumer):
+    # @database_sync_to_async
+    def get_vehicle_locations(self):
+        return VehicleLocation.objects.filter(latlong__within=self.bounds)
+
     def connect(self):
         self.accept()
+        print('connected')
 
     def disconnect(self, close_code):
+        print('disconnected')
         pass
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        bounds = json.loads(text_data)
+        self.bounds = Polygon.from_bbox(bounds)
+        print(self.bounds)
+        locations = self.get_vehicle_locations()
+        print(locations)
+        self.send(text_data=json.dumps(
+            [tuple(location.latlong) for location in locations]
+        ))
