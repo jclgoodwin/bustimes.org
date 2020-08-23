8c8f94a25984781951c4ec2299548e9f350f3c78
Josh Goodwin <j@joshuagoodw.in>
2020-08-23 15:32:51 +0100

    load more eagerly

--- busstops/static/js/global.js
+++ busstops/static/js/global.js
@@ -28,25 +28,9 @@ if (navigator.serviceWorker && location.protocol === 'https:') {
         return;
     }
 
-    function loadAds() {
-        window.adsbygoogle = (window.adsbygoogle || []);
-        if (document.cookie.indexOf('personalise_ads=yes') === -1) {
-            window.adsbygoogle.requestNonPersonalizedAds = 1;
-        }
-        var script = document.createElement('script');
-        script.dataset.adClient = 'ca-pub-4420219114164200';
-        script.async = 'async';
-        script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-        document.body.appendChild(script);
-
-        for (var i = ads.length - 1; i >= 0; i -= 1) {
-            window.adsbygoogle.push({});
-        }
-    }
-
     if (document.cookie.indexOf('seen_cookie_message=') === -1 && document.cookie.indexOf('personalise_ads=') === -1) {
         var cookieDiv = document.getElementById('cookie-message');
-        cookieDiv.innerHTML = '<p>Can we show you personalised advertisements, using cookies?</p>';
+        cookieDiv.innerHTML = '<p>Can we show you personalised advertisements, using <a href="/cookies">cookies</a>?</p>';
 
         var ok = document.createElement('button');
         ok.innerHTML = 'OK';
@@ -60,23 +44,33 @@ if (navigator.serviceWorker && location.protocol === 'https:') {
             cookieDiv.innerHTML = '<p>Great. If you change your mind you can use <a href="/cookies">change your settings</a>.</p>'
             cookieDiv.appendChild(ok);
             document.cookie = 'personalise_ads=yes; max-age=31536000; path=/';
-            loadAds();
         }
         cookieDiv.appendChild(yes);
 
         var no = document.createElement('button');
         no.innerHTML = 'No thanks';
         no.onclick = function() {
-            cookieDiv.innerHTML = '<p>Fair enough. You may see ads that are less relevant to you. These ads use cookies, but not for personalization.</p>';
+            cookieDiv.innerHTML = '<p>Fair enough. You may see ads that are less relevant to you. These ads use <a href="/cookies">cookies</a>, but not for personalization./p>';
             cookieDiv.appendChild(ok);
-            loadAds();
         }
         cookieDiv.appendChild(no);
 
         cookieDiv.style.display = 'block';
 
         document.cookie = 'personalise_ads=no; max-age=31536000; path=/';
-    } else {
-        loadAds()
+    }
+
+    window.adsbygoogle = (window.adsbygoogle || []);
+    if (document.cookie.indexOf('personalise_ads=yes') === -1) {
+        window.adsbygoogle.requestNonPersonalizedAds = 1;
+    }
+    var script = document.createElement('script');
+    script.dataset.adClient = 'ca-pub-4420219114164200';
+    script.async = 'async';
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    document.body.appendChild(script);
+
+    for (var i = ads.length - 1; i >= 0; i -= 1) {
+        window.adsbygoogle.push({});
     }
 })();
