320f865d1f9b4bffda7f76e46cb3dc667fb39771
Josh Goodwin <j@joshuagoodw.in>
2020-09-03 20:34:55 +0100

    add account urls

--- buses/urls.py
+++ buses/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('busstops.urls')),
 ]
 
