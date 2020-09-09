1e4e650e744e26e4bcef2587cc574c6871b86048
Josh Goodwin <j@joshuagoodw.in>
2020-09-09 15:30:07 +0100

    don't try minifying password reset email template

--- buses/utils.py
+++ buses/utils.py
@@ -6,6 +6,7 @@ import re
 def minify(template_source):
     """Alternative to django_template_minifier's minify function
     """
-    template_source = re.sub(r'(\n *)+', '\n', template_source)
-    template_source = re.sub(r'({%.+%})\n+', r'\1', template_source)
+    if '<' in template_source:
+        template_source = re.sub(r'(\n *)+', '\n', template_source)
+        template_source = re.sub(r'({%.+%})\n+', r'\1', template_source)
     return template_source
