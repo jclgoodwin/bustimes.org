cd835d38221fc16f196e455fedb6f264e0f7b658
Josh Goodwin <j@joshuagoodw.in>
2020-09-17 20:46:07 +0100

    fix test

--- bustimes/management/tests/test_import_transxchange.py
+++ bustimes/management/tests/test_import_transxchange.py
@@ -549,12 +549,14 @@ class ImportTransXChangeTest(TestCase):
 
         self.assertEqual(service.description, 'Glossop - Stalybridge - Ashton')
 
+        service.geometry = 'SRID=4326;MULTILINESTRING((1.31326925542 51.1278853356,1.08276947772 51.2766792559))'
+        service.save(update_fields=['geometry'])
+
         with freeze_time('1 September 2017'):
             with self.assertNumQueries(11):
                 res = self.client.get(service.get_absolute_url() + '?date=2017-09-01')
         self.assertEqual(str(res.context_data['timetable'].date), '2017-09-01')
         self.assertContains(res, 'Timetable changes from Sunday 3 September 2017')
-
         self.assertContains(res, f'data-service="{service.id},{duplicate.id}"></div')
 
         with freeze_time('1 October 2017'):
