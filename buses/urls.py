d18282233b37dd724ed71324f648102a9c3c15ca
Josh Goodwin <j@joshuagoodw.in>
2020-09-20 17:39:03 +0100

    simplify vehicle api a bit

--- buses/urls.py
+++ buses/urls.py
@@ -2,37 +2,34 @@ from django.urls import include, path
 from django.contrib import admin
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import routers, serializers, viewsets
-from vehicles.models import Vehicle, VehicleType
+from vehicles.models import Vehicle
 
 
-class VehicleSerializer(serializers.HyperlinkedModelSerializer):
+class VehicleSerializer(serializers.ModelSerializer):
     operator = serializers.SerializerMethodField()
     livery = serializers.SerializerMethodField()
 
     def get_operator(self, obj):
-        return obj.operator_id and {
-            'id': obj.operator_id,
-            'name': obj.operator.name,
-            'parent': obj.operator.parent,
-        }
+        if obj.operator_id:
+            return {
+                'id': obj.operator_id,
+                'name': obj.operator.name,
+                'parent': obj.operator.parent,
+            }
 
     def get_livery(self, obj):
-        return {
-            'name': obj.livery and str(obj.livery),
-            'left': obj.get_livery(),
-            'right': obj.get_livery(90)
-        }
+        if obj.colours or obj.livery_id:
+            return {
+                'id': obj.livery_id,
+                'name': obj.livery_id and str(obj.livery),
+                'left': obj.get_livery(),
+                'right': obj.get_livery(90)
+            }
 
     class Meta:
         model = Vehicle
         depth = 1
         exclude = ['code', 'source', 'latest_location', 'features', 'colours']
-
-
-class VehicleTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VehicleType
-        fields = '__all__'
 
 
 class VehicleViewSet(viewsets.ReadOnlyModelViewSet):
@@ -42,14 +39,8 @@ class VehicleViewSet(viewsets.ReadOnlyModelViewSet):
     filterset_fields = ['operator', 'vehicle_type', 'livery', 'withdrawn', 'reg']
 
 
-class VehicleTypeViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = VehicleType.objects.all()
-    serializer_class = VehicleTypeSerializer
-
-
 router = routers.DefaultRouter()
 router.register('vehicles', VehicleViewSet)
-router.register('types', VehicleTypeViewSet)
 
 
 urlpatterns = [
