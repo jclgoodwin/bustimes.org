00d19ae0613446f8b1c5166410f73193f720c09c
Josh Goodwin <j@joshuagoodw.in>
2020-09-24 16:00:02 +0000

    user admin

---
+++ accounts/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'last_login']
+
+
+admin.site.register(User, UserAdmin)
