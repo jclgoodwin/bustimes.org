38d55f425b47be4fdc494138e87c897a49d34d72
Josh Goodwin <j@joshuagoodw.in>
2020-09-25 14:00:06 +0100

    speed up update_search_indexes

--- busstops/management/commands/update_search_indexes.py
+++ busstops/management/commands/update_search_indexes.py
@@ -1,19 +1,24 @@
+from tqdm import tqdm
 from django.core.management.base import BaseCommand
+from django.db.models import Exists, OuterRef
 from ...models import Locality, Operator, Service
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        for locality in tqdm(Locality.objects.with_documents()):
+            locality.search_vector = locality.document
+            locality.save(update_fields=['search_vector'])
 
-        for locality in Locality.objects.all():
-            locality.update_search_vector()
+        has_services = Exists(Service.objects.filter(current=True, operator=OuterRef('pk')))
+        for operator in tqdm(Operator.objects.with_documents().filter(has_services)):
+            operator.search_vector = operator.document
+            operator.save(update_fields=['search_vector'])
 
-        for operator in Operator.objects.filter(service__current=True).distinct():
-            operator.update_search_vector()
+        print(Operator.objects.filter(~has_services).update(search_vector=None))
 
-        Operator.objects.exclude(service__current=True).update(search_vector=None)
+        for service in tqdm(Service.objects.with_documents().filter(current=True)):
+            service.search_vector = service.document
+            service.save(update_fields=['search_vector'])
 
-        for service in Service.objects.filter(current=True):
-            service.update_search_vector()
-
-        Service.objects.filter(current=False).update(search_vector=None)
+        print(Service.objects.filter(current=False).update(search_vector=None))
