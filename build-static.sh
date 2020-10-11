e8ef7cd02c60c7bc8f386f2bb64cb1a2aa237b85
Josh Goodwin <j@joshuagoodw.in>
2020-10-11 01:27:34 +0100

    makefile!

--- build-static.sh
+++
@@ -1,6 +0,0 @@
-./node_modules/.bin/sass --style=compressed busstops/static/css/style.scss busstops/static/css/style.css
-
-./node_modules/.bin/postcss busstops/static/css/style.css --map=false --use=autoprefixer -o busstops/static/css/style.css
-
-./node_modules/.bin/sass --style=compressed busstops/static/css/ie.scss busstops/static/css/ie.css
-
