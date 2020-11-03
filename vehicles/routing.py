7c705f7373527017218a88bb45e0c3555790a969
Josh Goodwin <j@joshuagoodw.in>
2020-11-03 13:38:47 +0000

    fix: support Django Channels 3.0

--- vehicles/routing.py
+++ vehicles/routing.py
@@ -3,6 +3,6 @@ from vehicles import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'^ws/vehicle_positions$', consumers.VehicleMapConsumer),
-    re_path(r'^ws/vehicle_positions/services/(?P<service_ids>[\d,]+)$', consumers.ServiceMapConsumer),
+    re_path(r'^ws/vehicle_positions$', consumers.VehicleMapConsumer.as_asgi()),
+    re_path(r'^ws/vehicle_positions/services/(?P<service_ids>[\d,]+)$', consumers.ServiceMapConsumer.as_asgi()),
 ]
