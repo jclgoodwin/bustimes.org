aaffc44544daaca91f8a6894f20d8ec983ed9781
Josh Goodwin <j@joshuagoodw.in>
2020-11-06 18:32:57 +0000

    make email (SMTP) host configurable

--- buses/settings.py
+++ buses/settings.py
@@ -18,9 +18,9 @@ DEFAULT_FROM_EMAIL = 'bustimes.org <contact@bustimes.org>'
 if TEST:
     EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 else:
-    EMAIL_HOST = 'smtp.fastmail.com'
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+    EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
     EMAIL_PORT = 465
     EMAIL_USE_SSL = True
     EMAIL_TIMEOUT = 10
