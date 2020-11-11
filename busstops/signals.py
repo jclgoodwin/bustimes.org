d806a875e40428dd2a65c0ba17520e9cf5347c0b
Josh Goodwin <j@joshuagoodw.in>
2020-11-11 17:22:15 +0000

    varnish ban on service link save

--- busstops/signals.py
+++ busstops/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.cache import cache
+from buses.utils import varnish_ban
 from .models import ServiceLink
 
 
@@ -10,3 +11,6 @@ def post_save_service_link(sender, instance, **kwargs):
     cache.delete(instance.from_service.get_similar_services_cache_key())
     cache.delete(instance.to_service.get_linked_services_cache_key())
     cache.delete(instance.to_service.get_similar_services_cache_key())
+
+    varnish_ban(instance.from_service.get_absolute_url())
+    varnish_ban(instance.to_service.get_absolute_url())
