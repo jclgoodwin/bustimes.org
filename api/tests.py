d7099c943085eced4b8d25f2dd5270e6014aa16b
Josh Goodwin <j@joshuagoodw.in>
2020-11-16 14:42:59 +0000

    fix

--- api/tests.py
+++ api/tests.py
@@ -4,6 +4,6 @@ from django.test import TestCase
 class ApiTest(TestCase):
     def test_api(self):
         with self.assertNumQueries(4):
-            response = self.client.get('/api/vehicles/', HTTP_ACCEPT='text/html')
-        self.assertContains(response, '<title>Vehicle List – API – bustimes.org</title>')
-        self.assertContains(response, '<a class='navbar-brand' href='/'>bustimes.org</a> ')
+            response = self.client.get("/api/vehicles/", HTTP_ACCEPT="text/html")
+        self.assertContains(response, "<title>Vehicle List – API – bustimes.org</title>")
+        self.assertContains(response, "<a class='navbar-brand' href='/'>bustimes.org</a> ")
