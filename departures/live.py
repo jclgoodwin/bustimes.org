f3f7a7229f05bfa740d0adbe48e1ef888dd90fa2
Josh Goodwin <j@joshuagoodw.in>
2021-01-10 01:03:28 +0000

    fix

--- departures/live.py
+++ departures/live.py
@@ -541,6 +541,7 @@ def get_departures(stop, services):
     routes = {}
     for route in Route.objects.filter(service__in=services).select_related('source'):
         if route.service_id in services:
+        if route.service_id in routes:
             routes[route.service_id].append(route)
         else:
             routes[route.service_id] = [route]
