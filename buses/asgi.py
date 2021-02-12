43d520f70b0b7ff76d3d424bcc2222c72d06cb86
Josh Goodwin <j@joshuagoodw.in>
2021-02-12 12:24:11 +0000

    sentry asgi middleware

--- buses/asgi.py
+++ buses/asgi.py
@@ -1,7 +1,10 @@
 import os
 import django
 from channels.routing import get_default_application
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buses.settings")
 django.setup()
 application = get_default_application()
+
+application = SentryAsgiMiddleware(application)
