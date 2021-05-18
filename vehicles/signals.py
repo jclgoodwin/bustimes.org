116d14555fd6fdeda84079629116ca7d31c9e5fb
Josh Goodwin <j@joshuagoodw.in>
2021-05-18 17:58:01 +0100

    int

--- vehicles/signals.py
+++ vehicles/signals.py
@@ -14,4 +14,4 @@ def vehicle_varnish_ban(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=Livery)
 def liveries_varnish_ban(sender, instance, **kwargs):
-    cache.set('liveries_css_version', datetime.utcnow().timestamp())
+    cache.set('liveries_css_version', int(datetime.utcnow().timestamp()))
