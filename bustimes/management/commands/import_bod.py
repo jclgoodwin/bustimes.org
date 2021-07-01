c9a6b27c9d464c587ce1b4d9e87160fdcf1a0e17
Josh Goodwin <j@joshuagoodw.in>
2021-07-01 17:21:20 +0100

    stagecoach: use 2.4 less (cos of incorrect future start dates in Fife data, etc)

--- bustimes/management/commands/import_bod.py
+++ bustimes/management/commands/import_bod.py
@@ -233,7 +233,7 @@ def stagecoach(operator=None):
             continue
 
         filename = f'stagecoach-{noc}-route-schedule-data-transxchange_2_4.zip'
-        if noc not in ('scek', 'sblb', 'sccm', 'sccu', 'scfi', 'schi', 'scem', 'sswl'):
+        if noc not in ('scek', 'sccm'):  # , 'sblb', 'sccu', 'scfi', 'schi', 'scem', 'sswl'):
             filename = filename.replace('_2_4', '')
         url = f'https://opendata.stagecoachbus.com/{filename}'
         path = os.path.join(settings.DATA_DIR, filename)
