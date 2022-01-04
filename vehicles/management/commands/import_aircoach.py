46ea219bc019f0294afb5adba6cad76751fe630d
Josh Goodwin <j@joshuagoodw.in>
2022-01-04 11:00:14 +0000

    remove kings ferry, fix Aircoach trip matching

--- vehicles/management/commands/import_aircoach.py
+++ vehicles/management/commands/import_aircoach.py
@@ -6,3 +6,9 @@ class Command(NatExpCommand):
     operators = ['663']
     url = 'https://tracker.aircoach.ie/api/eta/routes/{}/{}'
     sleep = 10
+
+    def get_journey(self, item, vehicle):
+        # reverse direction to aid matching of trips
+        item["dir"] = "I" if item["dir"] == "O" else "O"
+
+        return super().get_journey(item, vehicle)
--- vehicles/management/commands/import_kings_ferry.py
+++
@@ -1,8 +0,0 @@
-from .import_nx import Command as NatExpCommand
-
-
-class Command(NatExpCommand):
-    source_name = 'Kings Ferry'
-    operators = ['KNGF', 'CLKL']
-    url = 'https://kingsferry-tracker.utrackapps.com/api/eta/routes/{}/{}'
-    sleep = 10
