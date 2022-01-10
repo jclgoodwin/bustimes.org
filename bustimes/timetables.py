fbec61daa3468e695af14c3656f59344f995e5df
Josh Goodwin <j@joshuagoodw.in>
2022-01-10 17:24:41 +0000

    fix test, show future routes

--- bustimes/timetables.py
+++ bustimes/timetables.py
@@ -152,7 +152,10 @@ class Timetable:
             # self.calendars = ()
             return
 
-        routes = get_routes(routes, from_date=self.today)
+        if len(routes) > 1:
+            current_routes = get_routes(routes, from_date=self.today)
+            if len(current_routes) == 1:
+                routes = current_routes
 
         self.calendars = Calendar.objects.filter(
             Exists('trip', filter=Q(route__in=routes))
