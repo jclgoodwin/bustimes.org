jclgoodwin/bustimes.org

commit 8dea642fa2d344ac6e0ce1f378ea2c19c5b8c43b

show without-date-picker timetable for RA1, etc

bustimes/timetables.py
@@ -152,6 +152,8 @@ class Timetable:
             # self.calendars = ()
             return
 
+        routes = get_routes(routes, from_date=self.today)
+
         self.calendars = Calendar.objects.filter(
             Exists('trip', filter=Q(route__in=routes))
         ).annotate(

bustimes/models.py
@@ -6,9 +6,11 @@ from .fields import SecondsField
 from .utils import format_timedelta, time_datetime
 
 
-def get_routes(routes, when=None):
+def get_routes(routes, when=None, from_date=None):
     if when:
         routes = [route for route in routes if route.contains(when)]
+    if from_date:
+        routes = [route for route in routes if route.start_date is None or route.start_date <= from_date]
 
     if len(routes) == 1:
         return routes

commit 66677a4268ab2058a5462c8e726578cdd1fde384

fix regression introduced in 8dea642fa2d344ac6e0ce1f378ea2c19c5b8c43b https://twitter.com/parstree/status/1485921588285513731 needs test

bustimes/timetables.py
@@ -152,7 +152,7 @@ class Timetable:
             # self.calendars = ()
             return
 
-        if len(routes) > 1:
+        if not date and len(routes) > 1:
             current_routes = get_routes(routes, from_date=self.today)
             if len(current_routes) == 1:
                 routes = current_routes