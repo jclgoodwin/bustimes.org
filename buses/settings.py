d69c238712d60ea81e9754c40e32e2d4dba34981
Josh Goodwin <j@joshuagoodw.in>
2022-01-28 17:30:43 +0000

    Django 4.0: remove django-redis, use new built-in redis cache backend

--- buses/settings.py
+++ buses/settings.py
@@ -175,11 +175,8 @@ if TEST:
 elif REDIS_URL:
     CACHES = {
         "default": {
-            "BACKEND": "django_redis.cache.RedisCache",
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
             "LOCATION": REDIS_URL,
-            "OPTIONS": {
-                "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            }
         }
     }
 
