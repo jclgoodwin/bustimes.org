baa9e3b22bccc53979816810ce7b759945c3e706
Josh Goodwin <j@joshuagoodw.in>
2022-06-18 19:50:48 +0100

    fix Airoach tracking (primary key has changed)

--- vehicles/management/commands/import_aircoach.py
+++ vehicles/management/commands/import_aircoach.py
@@ -3,7 +3,7 @@ from .import_nx import Command as NatExpCommand
 
 class Command(NatExpCommand):
     source_name = 'Aircoach'
-    operators = ['663']
+    operators = ['ie-663']
     url = 'https://tracker.aircoach.ie/api/eta/routes/{}/{}'
     sleep = 10
 
