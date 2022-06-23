1ce4a1ad11826c0f6b241fe0740f4ed9087e506b
Josh Goodwin <j@joshuagoodw.in>
2022-06-23 17:52:10 +0100

    ohh

--- bustimes/management/tests/test_import_bod.py
+++ bustimes/management/tests/test_import_bod.py
@@ -58,6 +58,7 @@ class ImportBusOpenDataTest(TestCase):
                 OperatorCode(operator=lynx, source=source, code="LYNX"),
                 OperatorCode(operator=scpb, source=source, code="SCPB"),
                 OperatorCode(operator=schu, source=source, code="SCHU"),
+                OperatorCode(operator=schu, source=source, code="SCCM"),
             ]
         )
 
@@ -383,9 +384,6 @@ class ImportBusOpenDataTest(TestCase):
             url="https://opendata.stagecoachbus.com/stagecoach-sccm-route-schedule-data-transxchange.zip",
         )
         source.operators.add("SCCM")
-        nocs = DataSource.objects.create(name="National Operator Codes")
-        OperatorCode.objects.create(source=nocs, code="SCCM", operator_id="SCHU")
-        OperatorCode.objects.create(source=nocs, code="SCPB", operator_id="SCHU")
 
         StopPoint.objects.bulk_create(
             [
@@ -441,7 +439,7 @@ class ImportBusOpenDataTest(TestCase):
                     "bustimes.management.commands.import_bod.download_if_changed",
                     return_value=(True, parse_datetime("2020-06-10T12:00:00+01:00")),
                 ) as download_if_changed:
-                    with self.assertNumQueries(158):
+                    with self.assertNumQueries(153):
                         call_command("import_bod", "stagecoach")
                     download_if_changed.assert_called_with(
                         path, "https://opendata.stagecoachbus.com/" + archive_name
@@ -461,7 +459,7 @@ class ImportBusOpenDataTest(TestCase):
                     with self.assertNumQueries(1):
                         call_command("import_bod", "stagecoach", "SCOX")
 
-                    with self.assertNumQueries(97):
+                    with self.assertNumQueries(95):
                         call_command("import_bod", "stagecoach", "SCCM")
 
                     route_link.refresh_from_db()
