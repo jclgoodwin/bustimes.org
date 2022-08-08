abd5f8624803c6ab41d3f2e986ea2d9b4dd37aa3
Josh Goodwin <j@joshuagoodw.in>
2022-08-08 12:09:42 +0100

    frequency map: filter by time of day, divide by duration to get wavelength

--- bustimes/frequency_map.py
+++ bustimes/frequency_map.py
@@ -1,22 +1,19 @@
+from datetime import timedelta
+
 from django import forms
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.http import require_GET
+
 from busstops.models import StopPoint
 from .utils import get_calendars
-from .models import StopTime
+from .models import StopTime, RouteLink
 
 
 class Form(forms.Form):
     source = forms.IntegerField()
     date = forms.DateField()
-
-
-def get_color(frequency):
-    if frequency < 20:
-        return "red"
-    if frequency < 50:
-        return "blue"
-    return "green"
+    from_time = forms.TimeField()
+    to_time = forms.TimeField()
 
 
 @require_GET
@@ -29,13 +26,28 @@ def frequency_map(request):
 
     calendars = get_calendars(form.cleaned_data["date"])
 
+    from_time = form.cleaned_data["from_time"]
+    to_time = form.cleaned_data["to_time"]
+    from_time = timedelta(hours=from_time.hour, minutes=from_time.minute)
+    to_time = timedelta(hours=to_time.hour, minutes=to_time.minute)
+
     stop_times = StopTime.objects.filter(
         trip__route__source=form.cleaned_data["source"],
         trip__calendar__in=calendars,
+        trip__start__gte=from_time,
+        trip__start__lt=to_time,
     ).iterator()
+
+    duration = to_time - from_time
 
     pairs = {}
     stops = set()
+
+    route_links = RouteLink.objects.filter(service__source=form.cleaned_data["source"])
+    route_links = {
+        (route_link.from_stop_id, route_link.to_stop_id): route_link.geometry
+        for route_link in route_links
+    }
 
     previous = None
     for stop_time in stop_times:
@@ -64,19 +76,25 @@ def frequency_map(request):
                     "type": "Feature",
                     "geometry": {
                         "type": "LineString",
-                        "coordinates": [
+                        "coordinates": route_links[pair].coords
+                        if pair in route_links
+                        else [
                             locations[pair[0]].latlong.coords,
                             locations[pair[1]].latlong.coords,
                         ],
                     },
                     "properties": {
-                        "from": pair[0],
-                        "to": pair[1],
-                        "frequency": pairs[pair],
+                        # "from": pair[0],
+                        # "to": pair[1],
+                        "frequency": (duration / pairs[pair]).total_seconds()
+                        / 60,
                     },
-                    "style": {"stroke": get_color(pairs[pair])},
                 }
                 for pair in pairs
             ],
-        }
+        },
+        headers={
+            "access-control-allow-origin": "*",
+            "cache-control": "max-age=6000",
+        },
     )
