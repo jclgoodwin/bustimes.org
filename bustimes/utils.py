4284d8c31a9f3c219c8500b36712cd9f29c999cd
Josh Goodwin <j@joshuagoodw.in>
2022-09-07 17:25:26 +0100

    oop

--- bustimes/utils.py
+++ bustimes/utils.py
@@ -32,7 +32,7 @@ def get_routes(routes, when=None, from_date=None):
 
             if route.source.name.startswith(
                 "First Bus_"
-            ) or route.souce.name.startswith(
+            ) or route.source.name.startswith(
                 "National Express West Midlands"
             ):  # journeys may be split between sources (First Bristol)
                 route.key = f"{route.key}:{route.source_id}"
