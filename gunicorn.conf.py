eb434cf791c073131febdb8a372414816dbed8a2
Josh Goodwin <j@joshuagoodw.in>
2022-09-14 15:13:15 +0100

    remove max_requests

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,13 @@
-import multiprocessing
+# import multiprocessing
 
 bind = "0.0.0.0:8080"
 
-max_requests = 1000
-max_requests_jitter = 50
+# max_requests = 1000
+# max_requests_jitter = 50
 
 log_file = "-"
 
-workers = multiprocessing.cpu_count() * 2 + 1
+# workers = multiprocessing.cpu_count() * 2 + 1
 
 worker_tmp_dir = "/dev/shm"
 timeout = 30
