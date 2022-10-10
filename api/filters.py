ea59d82cc911296b840c2f7246bc84b5c45e25b1
Josh Goodwin <j@joshuagoodw.in>
2022-10-10 12:28:20 +0100

    api: add vehicle journey source filter, vehicle slug filter

--- api/filters.py
+++ api/filters.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from django_filters.rest_framework import FilterSet, CharFilter, NumberFilter
+from django_filters.rest_framework import CharFilter, FilterSet, NumberFilter
 
 from busstops.models import StopPoint
 from vehicles.models import Vehicle
@@ -9,6 +9,7 @@ class VehicleFilter(FilterSet):
     search = CharFilter(method="search_filter", label="Search")
     fleet_code = CharFilter(lookup_expr="iexact")
     reg = CharFilter(lookup_expr="iexact")
+    slug = CharFilter()
     operator = CharFilter()
 
     def search_filter(self, queryset, name, value):
@@ -23,6 +24,7 @@ class VehicleJourneyFilter(FilterSet):
     vehicle = NumberFilter()
     service = NumberFilter()
     trip = NumberFilter()
+    source = NumberFilter()
 
 
 class StopFilter(FilterSet):
