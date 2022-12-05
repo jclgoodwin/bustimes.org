45c2bc10a7935d46c96652d7ce8135e9edd51f44
Josh Goodwin <j@joshuagoodw.in>
2022-12-05 11:34:17 +0000

    remove item from cache when updating vehicle (livery etc), so the map icon will update tout suite

--- vehicles/signals.py
+++ vehicles/signals.py
@@ -2,9 +2,15 @@ from django.core.cache import cache
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Livery
+from .models import Livery, Vehicle
 
 
 @receiver(post_save, sender=Livery)
 def liveries_cache_update(sender, instance, **kwargs):
     cache.set("liveries_css_version", int(instance.updated_at.timestamp()), None)
+
+
+@receiver(post_save, sender=Vehicle)
+def vehicle_cache_update(sender, instance, **kwargs):
+    if instance.latest_journey_id:
+        cache.delete(f"journey{instance.latest_journey_id}")
