a3c6e1e38c15a7221aa59077c09ddd2614c48238
Josh Goodwin <j@joshuagoodw.in>
2023-03-28 16:12:37 +0100

    debounce

--- busstops/static/js/bigmap2.js
+++ busstops/static/js/bigmap2.js
@@ -1,8 +1,22 @@
+/*
+ * jQuery throttle / debounce - v1.1 - 3/7/2010
+ * http://benalman.com/projects/jquery-throttle-debounce-plugin/
+ *
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+/* eslint-disable semi */
+/* eslint-disable quotes */
+(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
+/* eslint-enable semi */
+/* eslint-enable quotes */
+
 (function () {
     'use strict';
 
     /*global
-        L, reqwest, bustimes
+        L, reqwest, bustimes, Cowboy
     */
 
     var map = L.map('hugemap', {
@@ -328,7 +342,7 @@
     var first = true;
     var mapContainer = map.getContainer();
 
-    map.on('moveend', function() {
+    map.on('moveend', Cowboy.debounce(500, function() {
         if (!first) {
             loadVehicles(true);
         }
@@ -353,7 +367,7 @@
         updateLocation();
 
         first = false;
-    });
+    }));
 
     var parts;
     if (location.hash) {
