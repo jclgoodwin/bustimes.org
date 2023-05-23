47c473ce60768a0cf528b67989db1606abb58e8b
Josh Goodwin <j@joshuagoodw.in>
2023-05-23 12:54:12 +0100

    delete licenses and registrations no longer appearing in the otc data (seems to be expired licences) might regret this

--- vosa/management/commands/import_vosa.py
+++ vosa/management/commands/import_vosa.py
@@ -293,3 +293,11 @@ class Command(BaseCommand):
             ],
             batch_size=1000,
         )
+
+        for reg in regs.values():
+            if reg not in regs_to_create and reg not in regs_to_update:
+                print(reg.registration_number, reg.delete())
+
+        for lic in lics.values():
+            if lic not in lics_to_update and lic not in lics_to_update:
+                print(lic.licence_number, lic.delete())
