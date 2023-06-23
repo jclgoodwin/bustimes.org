58d65f452cc4d29762c1c09f37ffbf01b40ed162
Josh Goodwin <j@joshuagoodw.in>
2023-06-23 09:49:54 +0100

    stagecoach: update garage refs, and use final stop name if destination display is blank (scotland)

--- vehicles/management/commands/import_stagecoach.py
+++ vehicles/management/commands/import_stagecoach.py
@@ -151,7 +151,8 @@ class Command(ImportLiveVehiclesCommand):
 
         journey = VehicleJourney(
             datetime=departure_time,
-            destination=item.get("dd", ""),  # destinationDisplay
+            destination=item.get("dd", "")
+            or item.get("fs", ""),  # destinationDisplay or finalStopName
             route_name=item.get("sn", ""),  # serviceNumber
         )
 
@@ -203,6 +204,12 @@ class Command(ImportLiveVehiclesCommand):
             journey.trip = journey.get_trip(
                 destination_ref=item.get("fr"), departure_time=departure_time
             )
+
+            # update vehicle garage
+            if trip := journey.trip:
+                if trip.garage_id != vehicle.garage_id:
+                    vehicle.garage_id = trip.garage_id
+                    vehicle.save(update_fields=["garage"])
 
         return journey
 
