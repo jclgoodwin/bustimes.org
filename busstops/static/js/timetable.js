526b79b65e3fc7e54e2abd90776837e1cfe7f3aa
Josh Goodwin <j@joshuagoodw.in>
2023-07-18 17:25:00 +0100

    oops

---
+++ busstops/static/js/timetable.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/*jslint
+    browser: true
+*/
+/*global
+    reqwest, SERVICE_ID
+*/
+
+(function() {
+    var timetableWrapper = document.getElementById('timetable');
+
+    function doStuff() {
+        if (document.querySelectorAll && document.referrer.indexOf('/stops/') > -1) {
+            var referrer = document.referrer.split('/stops/')[1];
+            referrer = referrer.split('?')[0];
+            if (referrer) {
+                // highlight the row of the referring stop
+                document.querySelectorAll('tr th a[href="/stops/' + referrer + '"]').forEach(function(a) {
+                    a.parentNode.parentNode.className += ' referrer';
+                });
+            }
+        }
+
+        // load timetable for a particular date without loading the rest of the page
+        if (timetableWrapper) {
+            var timetableForm = timetableWrapper.querySelector('form');
+        }
+        if (timetableForm) {
+            timetableForm.onchange = function() {
+                timetableWrapper.className = 'loading';
+
+                var formData = new FormData(timetableForm);
+                var newSearch = '?' + new URLSearchParams(formData).toString();
+                reqwest('/services/' + SERVICE_ID + '/timetable' + newSearch, function(response) {
+                    timetableWrapper.className = '';
+                    timetableWrapper.innerHTML = response;
+                    doStuff();
+                    search = newSearch;
+                    history.pushState(null, null, newSearch);
+                });
+            };
+        }
+
+    }
+
+    doStuff();
+
+    var search  = window.location.search;
+
+    window.addEventListener('popstate', function() {
+        // handle back/forward navigation
+        if (search !== window.location.search) {
+            search = window.location.search;
+            var url = '/services/' + SERVICE_ID + '/timetable';
+            if (search) {
+                url += search;
+            }
+            timetableWrapper.className = 'loading';
+            reqwest(url, function(response) {
+                timetableWrapper.className = '';
+                timetableWrapper.innerHTML = response;
+                doStuff();
+            });
+        }
+    });
+}());
