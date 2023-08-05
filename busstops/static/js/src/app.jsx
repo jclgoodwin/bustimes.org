65f4d1b6cff5083421cf9d2e6c3640e4273c1e68
Josh Goodwin <j@joshuagoodw.in>
2023-08-05 01:06:26 +0100

    sentry allowUrls setting: filter out some noise from ad code

--- busstops/static/js/src/app.jsx
+++ busstops/static/js/src/app.jsx
@@ -10,7 +10,7 @@ const History = lazy(() => import("./History"));
 
 Sentry.init({
   dsn: "https://0d628b6fff45463bb803d045b99aa542@o55224.ingest.sentry.io/1379883",
-  // allowUrls: [/bustimes\.org\/static\//],
+  allowUrls: [/bustimes\.org\/static\//],
 });
 
 import "./maps.css";
