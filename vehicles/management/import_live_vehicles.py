adc09db5a8f4e84701b6f67f6c64e575d638f7de
Josh Goodwin <j@joshuagoodw.in>
2023-08-18 11:46:40 +0100

    more trying to fix duplicate ticketer journeys

--- vehicles/management/import_live_vehicles.py
+++ vehicles/management/import_live_vehicles.py
@@ -220,9 +220,7 @@ class ImportLiveVehiclesCommand(BaseCommand):
 
         if keep_journey:
             pass
-        elif keep_journey is None and same_journey(
-            latest_journey, journey, latest_datetime, location.datetime
-        ):
+        elif same_journey(latest_journey, journey, latest_datetime, location.datetime):
             changed = []
             if latest_journey.source_id != self.source.id:
                 latest_journey.source = self.source
