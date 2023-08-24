6a9400c66594792a658cb2fb69dd515dc38a2b38
Josh Goodwin <j@joshuagoodw.in>
2023-08-24 13:28:07 +0100

    try a non-split version of the js for pages where splitting is not needed

---
+++ busstops/static/js/src/map.tsx
@@ -0,0 +1,60 @@
+import React, { lazy } from "react";
+import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
+
+import "./maps.css";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { Trip } from "./TripTimetable";
+import BigMap from "./BigMap";
+import TripMap from "./TripMap";
+import OperatorMap from "./OperatorMap";
+
+Sentry.init({
+  dsn: "https://0d628b6fff45463bb803d045b99aa542@o55224.ingest.sentry.io/1379883",
+  allowUrls: [/bustimes\.org\/static\//],
+  ignoreErrors: [
+    "TypeError: Failed to fetch",
+    "TypeError: Load failed",
+    "AbortError: The user aborted a request",
+    "AbortError: Fetch is aborted",
+    "NetworkError when attempting to fetch resource",
+    "Non-Error promise rejection captured with value: undefined",
+    "from accessing a cross-origin frame. Protocols, domains, and ports must"
+  ],
+});
+
+declare global {
+  interface Window {
+    STOPS: Trip;
+    OPERATOR_ID: string;
+  }
+}
+
+let rootElement = document.getElementById("hugemap");
+if (rootElement) {
+  let root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <BigMap />
+    </React.StrictMode>,
+  );
+} else {
+  rootElement = document.getElementById("map");
+  if (rootElement) {
+    if (window.location.href.indexOf("/operators/") !== -1) {
+      let root = ReactDOM.createRoot(rootElement);
+      root.render(
+        <React.StrictMode>
+          <OperatorMap noc={window.OPERATOR_ID} />
+        </React.StrictMode>,
+      );
+    } else if (window.STOPS) {
+      let root = ReactDOM.createRoot(rootElement);
+      root.render(
+        <React.StrictMode>
+          <TripMap />
+        </React.StrictMode>,
+      );
+    }
+  }
+}
