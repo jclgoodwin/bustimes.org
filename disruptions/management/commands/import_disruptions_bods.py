7ee3db89c151ba4174003cafa7b376b7475876e5
Josh Goodwin <j@joshuagoodw.in>
2023-09-23 13:49:41 +0100

    add bods disruptions importer

---
+++ disruptions/management/commands/import_disruptions_bods.py
@@ -0,0 +1,43 @@
+import io
+import xml.etree.cElementTree as ET
+import zipfile
+
+import requests
+from django.core.management.base import BaseCommand
+
+from busstops.models import DataSource
+
+from .import_siri_sx import handle_item
+
+
+class Command(BaseCommand):
+    def fetch(self):
+        url = "https://data.bus-data.dft.gov.uk/disruptions/download/bulk_archive"
+
+        source = DataSource.objects.get_or_create(name="Bus Open Data Service")[0]
+
+        situations = []
+
+        response = requests.get(url, timeout=10)
+        assert response.ok
+
+        archive = zipfile.ZipFile(io.BytesIO(response.content))
+
+        namelist = archive.namelist()
+        assert len(namelist) == 1
+        open_file = archive.open(namelist[0])
+
+        for _, element in ET.iterparse(open_file):
+            if element.tag[:29] == "{http://www.siri.org.uk/siri}":
+                element.tag = element.tag[29:]
+
+            if element.tag.endswith("PtSituationElement"):
+                situations.append(handle_item(element, source))
+                element.clear()
+
+        source.situation_set.filter(current=True).exclude(id__in=situations).update(
+            current=False
+        )
+
+    def handle(self, *args, **options):
+        self.fetch()
