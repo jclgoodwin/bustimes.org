d024321bc84c89e6628408280a4e682768c36d26
Josh Goodwin <j@joshuagoodw.in>
2023-11-11 16:49:55 +0000

    suggest_bod: prettier output

--- bustimes/management/commands/suggest_bod.py
+++ bustimes/management/commands/suggest_bod.py
@@ -1,5 +1,10 @@
 import requests
+import json
+from datetime import datetime
 from django.core.management.base import BaseCommand
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import JsonLexer
 from time import sleep
 from busstops.models import Service
 from .import_transxchange import get_open_data_operators
@@ -17,8 +22,10 @@ class Command(BaseCommand):
 
         session = requests.Session()
 
+        formatter = TerminalFormatter()
+
         url = "https://data.bus-data.dft.gov.uk/api/v1/dataset/"
-        params = {"api_key": api_key, "status": ["published", "expiring"], "limit": 100}
+        params = {"api_key": api_key, "status": ["published"], "limit": 100, "modifiedDate": "2023-01-01T00:00:00", "endDateStart": datetime.now().isoformat()}
         while url:
             response = session.get(url, params=params)
             print(response.url)
@@ -30,12 +37,8 @@ class Command(BaseCommand):
                     continue
                 if any(noc in open_data_operators for noc in item["noc"]):
                     continue
-                print(item["name"])
-                print(" ", item["noc"])
-                print(" ", item["description"])
-                print(" ", item["comment"])
-                print(" ", item["adminAreas"])
-                print(" ", item["url"])
+                del item["localities"]
+                print(highlight(json.dumps(item, indent=4), JsonLexer(), formatter))
             url = data["next"]
             params = None
             if url:
