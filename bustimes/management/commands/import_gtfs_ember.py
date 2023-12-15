5a72d074a88fb5779646f2a495871d51c2b188e1
Josh Goodwin <j@joshuagoodw.in>
2023-12-15 13:53:27 +0000

    Ember GTFS: cope with stop ids not present in NaPTAN, etc

--- bustimes/management/commands/import_gtfs_ember.py
+++ bustimes/management/commands/import_gtfs_ember.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
-from busstops.models import DataSource, Operator, Service
+from busstops.models import DataSource, Operator, Service, StopPoint
 
 from ...download_utils import download_if_changed
 from ...models import Calendar, CalendarDate, Route, StopTime, Trip
@@ -74,9 +74,11 @@ class Command(BaseCommand):
         existing_routes = {route.code: route for route in source.route_set.all()}
         routes = []
 
+        stops = StopPoint.objects.in_bulk(feed.stops.stop_id.to_list())
+
         calendars = get_calendars(feed)
 
-        for i, row in feed.routes.iterrows():
+        for i, row in gtfs_kit.routes.geometrize_routes(feed).iterrows():
             if row.route_id in existing_services:
                 service = existing_services[row.route_id]
             else:
@@ -90,8 +92,10 @@ class Command(BaseCommand):
             route.line_name = row.route_id
             service.description = route.description = row.route_long_name
             service.current = True
+            service.geometry = row.geometry.wkt
 
             service.save()
+            service.operator.add(operator)
             route.save()
 
             routes.append(route)
@@ -118,18 +122,21 @@ class Command(BaseCommand):
             if not trip.start:
                 trip.start = row.arrival_time
             trip.end = row.departure_time
-            trip.destination_id = row.stop_id
 
-            stop_times.append(
-                StopTime(
-                    stop_id=row.stop_id,
-                    arrival=row.arrival_time,
-                    departure=row.departure_time,
-                    sequence=row.stop_sequence,
-                    trip=trip,
-                    timing_status="PTP" if row.timepoint else "OTH",
-                )
+            stop_time = StopTime(
+                arrival=row.arrival_time,
+                departure=row.departure_time,
+                sequence=row.stop_sequence,
+                trip=trip,
+                timing_status="PTP" if row.timepoint else "OTH",
             )
+
+            stop_time.stop = trip.destination = stops.get(row.stop_id)
+
+            if stop_time.stop is None:
+                stop_time.stop_code = row.stop_id
+
+            stop_times.append(stop_time)
 
         with transaction.atomic():
             Trip.objects.bulk_create([trip for trip in trips.values() if not trip.id])
@@ -149,6 +156,10 @@ class Command(BaseCommand):
 
             StopTime.objects.filter(trip__in=existing_trips).delete()
             StopTime.objects.bulk_create(stop_times)
+
+            for service in source.service_set.filter(current=True):
+                service.do_stop_usages()
+                service.update_search_vector()
 
             print(
                 source.route_set.exclude(id__in=[route.id for route in routes]).delete()
