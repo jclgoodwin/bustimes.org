a5c58e893851836a2fc3a98556891dc59a02b04d
Josh Goodwin <j@joshuagoodw.in>
2024-01-18 14:32:24 +0000

    test flixbus stop code

--- bustimes/management/tests/test_import_gtfs_ember_flixbus.py
+++ bustimes/management/tests/test_import_gtfs_ember_flixbus.py
@@ -6,7 +6,7 @@ import time_machine
 from django.core.management import call_command
 from django.test import TestCase, override_settings
 
-from busstops.models import DataSource, Operator, Region, Service, StopPoint
+from busstops.models import DataSource, Operator, Region, Service, StopCode, StopPoint
 
 from ...models import Route, Trip
 
@@ -36,7 +36,7 @@ class FlixbusTest(TestCase):
         service = Service.objects.create(line_name="004")
         service.operator.add("FLIX")
         route = Route.objects.create(
-            line_name="004", code="004", service=service, source=sources[1]
+            line_name="004", code="UK004", service=service, source=sources[1]
         )
         Trip.objects.create(
             route=route,
@@ -63,6 +63,14 @@ class FlixbusTest(TestCase):
 
         StopPoint.objects.create(
             atco_code="6200247603", common_name="Aeropuerto d'Edinburgh", active=1
+        )
+        StopPoint.objects.create(
+            atco_code="3390C11", common_name="Nottingham", active=1
+        )
+        StopCode.objects.create(
+            source=sources[1],
+            code="9b69e4fe-3ecb-11ea-8017-02437075395e",
+            stop_id="3390C11",
         )
 
     def test_not_modified(self):
@@ -94,12 +102,15 @@ class FlixbusTest(TestCase):
         self.assertContains(
             response, "<td>10:30</td><td>15:00</td><td>19:15</td><td>23:40</td>"
         )
+        self.assertContains(response, "/stops/3390C11")
 
         # British Summer Time:
         response = self.client.get(f"{service.get_absolute_url()}?date=2024-04-01")
         self.assertContains(
             response, "<td>10:30</td><td>15:00</td><td>19:15</td><td>23:40</td>"
         )
+
+        self.assertEqual(Service.objects.all().count(), 3)
 
     @time_machine.travel("2023-01-01")
     def test_import_gtfs_ember(self):
@@ -110,6 +121,7 @@ class FlixbusTest(TestCase):
                 datetime.datetime(2024, 6, 18, 10, 0, 0, tzinfo=datetime.timezone.utc),
             ),
         ):
+            call_command("import_gtfs_ember")
             call_command("import_gtfs_ember")
 
         response = self.client.get("/operators/ember")
