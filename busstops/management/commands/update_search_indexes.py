e17da08370e34ccfba277477cb0fc7d36f102c5f
Josh Goodwin <j@joshuagoodw.in>
2024-06-07 13:30:05 +0100

    better update_search_indexes

--- busstops/management/commands/update_search_indexes.py
+++ busstops/management/commands/update_search_indexes.py
@@ -5,28 +5,16 @@ from ...models import Locality, Operator, Service
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        to_update = []
 
-        for locality in Locality.objects.with_documents():
-            if locality.search_vector != locality.document:
-                locality.search_vector = locality.document
-                to_update.append(locality)
+        for queryset in (
+            Locality.objects.with_documents(),
+            Operator.objects.with_documents(),
+            Service.objects.with_documents().filter(current=True),
+        ):
+            to_update = []
+            for item in queryset:
+                if item.search_vector != item.document:
+                    item.search_vector = item.document
+                    to_update.append(item)
 
-        Locality.objects.bulk_update(to_update, ["search_vector"])
-        to_update = []
-
-        for operator in Operator.objects.with_documents():
-            if operator.search_vector != operator.document:
-                operator.search_vector = operator.document
-                to_update.append(locality)
-
-        Operator.objects.bulk_update(to_update, ["search_vector"])
-        to_update = []
-
-        for service in Service.objects.with_documents().filter(current=True):
-            if service.search_vector != service.document:
-                service.search_vector = service.document
-                to_update.append(service)
-
-        Service.objects.bulk_update(to_update, ["search_vector"])
-        to_update = []
+            queryset.bulk_update(to_update, ["search_vector"])
