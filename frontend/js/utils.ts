5bf4f08625091c4d6051c9d76a5ac8d87abbbda0
Josh Goodwin <j@joshuagoodw.in>
2024-07-24 12:34:00 +0100

    dark mode again

--- frontend/js/utils.ts
+++ frontend/js/utils.ts
@@ -1,23 +1,21 @@
+import { useState, useEffect } from "react";
+
 export const useDarkMode = () => {
-  /*
-  if (window.matchMedia) {
-    const query = window.matchMedia("(prefers-color-scheme: dark)");
+  const query = window.matchMedia("(prefers-color-scheme: dark)");
 
-    const [darkMode, setDarkMode] = useState(query.matches);
+  const [darkMode, setDarkMode] = useState(query.matches);
 
-    useEffect(() => {
-      const handleChange = (e) => {
-        setDarkMode(e.matches);
-      };
+  useEffect(() => {
+    const handleChange = (e) => {
+      setDarkMode(e.matches);
+    };
 
-      query.addEventListener("change", handleChange);
+    query.addEventListener("change", handleChange);
 
-      return () => {
-        query.removeEventListener("change", handleChange);
-      };
-    }, []);
-  }
-  */
+    return () => {
+      query.removeEventListener("change", handleChange);
+    };
+  }, [query]);
 
-  return false;
+  return darkMode
 };
