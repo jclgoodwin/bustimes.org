95cbbaf7fc173b6f8c097e76d789e36affca4059
Glade <gladeethewolf@icloud.com>
2025-03-08 09:31:24 +0000

    Update settings.py

--- buses/settings.py
+++ buses/settings.py
@@ -15,7 +15,7 @@ from sentry_sdk.integrations.redis import RedisIntegration
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS", "[::1] 127.0.0.1 localhost 16.local gladetf.mrlift.xyz"
+    "ALLOWED_HOSTS", "[::1] 127.0.0.1 localhost 16.local gladetf.mrlift.xyz 192.1.168.150"
 ).split()
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
