70aaf7b4fa3682fe0cfca12ecc5f9fbacded28df
Josh Goodwin <j@joshuagoodw.in>
2025-05-03 08:19:38 +0000

    more liberal ALLOWED_HOSTS (for kamal health check etc)

--- buses/settings.py
+++ buses/settings.py
@@ -15,7 +15,7 @@ from sentry_sdk.integrations.redis import RedisIntegration
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS", "[::1] 127.0.0.1 localhost 16.local"
+    "ALLOWED_HOSTS", "*"
 ).split()
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
