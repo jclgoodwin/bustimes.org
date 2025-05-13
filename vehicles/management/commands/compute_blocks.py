5d2f7e4e0efb72a215929ab6b9fd99f2641b4ef6
Josh Goodwin <j@joshuagoodw.in>
2025-05-13 08:01:55 +0000

    hot fixes

--- vehicles/management/commands/compute_blocks.py
+++ vehicles/management/commands/compute_blocks.py
@@ -19,27 +19,29 @@ class Command(BaseCommand):
 
         block_number = 1
 
+        Trip.objects.filter(operator=operator_code).update(block='')
+
         for v in vehicles.filter(
             Exists("vehiclejourney", filter=Q(datetime__date=yesterday))
         ):
             journeys = v.vehiclejourney_set.filter(datetime__date=yesterday)
             trip_ids = [j.trip_id for j in journeys]
             if trip_ids[0]:
-                previous_vehicle = vehicles.filter(
+                for previous_vehicle in vehicles.filter(
                     Exists(
                         "vehiclejourney",
                         filter=Q(datetime__date=before_yesterday, trip=trip_ids[0]),
                     )
-                ).get()
-                previous_journeys = previous_vehicle.vehiclejourney_set.filter(
-                    datetime__date=yesterday
-                )
-                previous_trip_ids = [j.trip_id for j in previous_journeys]
-
-                print(trip_ids)
-                print(previous_trip_ids)
-                if trip_ids == previous_trip_ids:
-                    Trip.objects.filter(id__in=trip_ids).update(
-                        block_number=block_number
+                ):
+                    previous_journeys = previous_vehicle.vehiclejourney_set.filter(
+                        datetime__date=yesterday
                     )
-                    block_number += 1
+                    previous_trip_ids = [j.trip_id for j in previous_journeys]
+
+                    print(trip_ids)
+                    print(previous_trip_ids)
+                    if trip_ids == previous_trip_ids:
+                        Trip.objects.filter(id__in=trip_ids).update(
+                            block=block_number
+                        )
+                        block_number += 1
