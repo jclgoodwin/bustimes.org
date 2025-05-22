2f1b1da212d1c16f06a09b8c679945aa815653a8
Josh Goodwin <j@joshuagoodw.in>
2025-05-22 17:20:45 +0000

    fetch disruptions more often

--- disruptions/tasks.py
+++ disruptions/tasks.py
@@ -3,7 +3,7 @@ from huey.contrib.djhuey import db_periodic_task
 
 from . import siri_sx, tfl_disruptions
 
-bods_disruptions = db_periodic_task(crontab(minute="*/20"))(siri_sx.bods_disruptions)
-tfl_disruptions_task = db_periodic_task(crontab(minute="*/20"))(
+bods_disruptions = db_periodic_task(crontab(minute="*/6"))(siri_sx.bods_disruptions)
+tfl_disruptions_task = db_periodic_task(crontab(minute="*/6"))(
     tfl_disruptions.tfl_disruptions
 )
