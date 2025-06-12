fc3c7e6acbfe75c71dd99a2bdc5eb1223330c2b0
Josh Goodwin <j@joshuagoodw.in>
2025-06-12 13:44:20 +0100

    "newport"

---
+++ vehicles/management/commands/newport.py
@@ -0,0 +1,54 @@
+from ciso8601 import parse_datetime
+from django.contrib.gis.geos import GEOSGeometry
+from ...models import VehicleLocation, VehicleJourney
+from ..import_live_vehicles import ImportLiveVehiclesCommand
+
+
+class Command(ImportLiveVehiclesCommand):
+    source_name = "guernsey"
+
+    def get_items(self):
+        response = self.session.get(self.source.url, **self.source.settings)
+        return response.json().get("items")
+
+    @staticmethod
+    def get_datetime(item):
+        return parse_datetime(item["reported"])
+
+    def get_vehicle(self, item):
+        print(item)
+        code = item["vehicleRef"]
+        defaults = {"fleet_code": code}
+        if code.isdigit():
+            defaults["fleet_number"] = code
+
+        return self.vehicles.get_or_create(defaults, code=code, operator_id="SGUE")
+
+    def get_journey(self, item, vehicle):
+        datetime = parse_datetime(item["scheduledTripStartTime"])
+
+        latest_journey = vehicle.latest_journey
+        if latest_journey and latest_journey.datetime == datetime:
+            return latest_journey
+        else:
+            try:
+                return vehicle.vehiclejourney_set.get(datetime=datetime)
+            except VehicleJourney.DoesNotExist:
+                journey = VehicleJourney(datetime=datetime)
+
+        journey.route_name = item["routeName"]
+        journey.destination = item.get("destination", "")
+
+        return journey
+
+    def create_vehicle_location(self, item):
+        position = item["position"]
+        bearing = position.get("bearing")
+        if bearing == "-1":
+            bearing = None
+        return VehicleLocation(
+            latlong=GEOSGeometry(
+                f"POINT({position['longitude']} {position['latitude']})"
+            ),
+            heading=bearing,
+        )
