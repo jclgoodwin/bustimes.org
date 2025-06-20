6985ab4e262e6f4bde75e019d9fdfd86991dbff7
Dylan <dylanjtoner@gmail.com>
2025-06-20 10:49:28 +0100

    debug tests

--- bustimes/management/tests/test_import_bod.py
+++ bustimes/management/tests/test_import_bod.py
@@ -191,7 +191,8 @@ Lynx/Bus Open Data Service (BODS)</a>, <time datetime="2020-04-01">1 April 2020<
         response = self.client.get(f"/api/trips/?service={route.service_id}")
         self.assertEqual(4, len(response.json()["results"]))
 
-        response = self.client.get(f"/api/trips/?date={route.start_date}")
+        response = self.client.get(f"/api/trips/?date=2020-04-01")
+        print(response.json()["results"])
         self.assertEqual(4, len(response.json()["results"]))
 
         response = self.client.get(trip.get_absolute_url())
