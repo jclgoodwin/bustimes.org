4011b3e6e701925739d4b16774bbdc0de097bc50
Josh Goodwin <j@joshuagoodw.in>
2025-07-11 02:50:00 +0100

    stops admin: add source filter

--- busstops/admin.py
+++ busstops/admin.py
@@ -40,6 +40,7 @@ class StopPointAdmin(GISModelAdmin):
     ]
     list_select_related = ["locality", "admin_area"]
     list_filter = [
+        ("source", admin.RelatedOnlyFieldListFilter),
         "modified_at",
         "created_at",
         "active",
