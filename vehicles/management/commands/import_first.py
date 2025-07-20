070decd0d53508dde90b7e05283ab76a258f0fbe
Josh Goodwin <j@joshuagoodw.in>
2025-07-20 10:11:51 +0100

    log and better handle strangeness

--- vehicles/management/commands/import_first.py
+++ vehicles/management/commands/import_first.py
@@ -15,7 +15,7 @@ from websockets.asyncio.client import connect
 from busstops.models import DataSource, Operator, Service
 
 from ...models import Vehicle, VehicleJourney, VehicleLocation
-from ..import_live_vehicles import ImportLiveVehiclesCommand
+from ..import_live_vehicles import ImportLiveVehiclesCommand, logger
 
 
 class Command(ImportLiveVehiclesCommand):
@@ -120,16 +120,28 @@ class Command(ImportLiveVehiclesCommand):
         self.to_save.append((location, vehicle))
 
     @staticmethod
-    def split_vehicle_id(item: dict) -> (str, str):
+    def split_vehicle_id(item: dict):
         prefix = f"{item['operator']}-{item['dir']}-"
         suffix = f"-{item['line_name']}"
         vehicle = item["status"]["vehicle_id"]
 
-        assert vehicle.startswith(prefix)
-        assert vehicle.endswith(suffix)
-        vehicle = vehicle.removesuffix(suffix).removeprefix(prefix)
-        journey, vehicle = vehicle[11:].split("-", 1)
-        return journey, vehicle
+        if vehicle.startswith(prefix) and vehicle.endswith(suffix):
+            vehicle = vehicle.removesuffix(suffix).removeprefix(prefix)
+            return vehicle[11:].split("-", 1)
+        else:
+            logger.warning(
+                "vehicle %s doesn't have prefix %s and/or suffix %s",
+                vehicle,
+                prefix,
+                suffix,
+                exc_info=True,
+            )
+            parts = vehicle.split("-")
+            assert len(parts) >= 8
+            item["line_name"] = parts[-1]
+            item["operator"] = parts[0]
+            item["dir"] = parts[1]
+            return parts[5], "-".join(parts[6:-1])
 
     @sync_to_async
     def handle_data(self, data):
