5afa18bb654e19a6caea68d5d5e75d4e651a08a1
Josh Goodwin <j@joshuagoodw.in>
2025-08-12 10:30:41 +0100

    test websocket

--- buses/asgi.py
+++ buses/asgi.py
@@ -1,8 +1,8 @@
 import os
-
-from channels.routing import ProtocolTypeRouter
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from vehicles.consumers import MyConsumer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buses.settings")
 
@@ -13,6 +13,10 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        # Just HTTP for now. (We can add other protocols later.)
+        "websocket": URLRouter(
+            [
+                path("vehicles/<int:id>", MyConsumer.as_asgi()),
+            ]
+        ),
     }
 )
