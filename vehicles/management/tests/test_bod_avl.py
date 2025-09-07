9c8cfb1266e89110dd96ae3e6de03c34992292de
Josh Goodwin <j@joshuagoodw.in>
2025-09-07 12:10:03 +0100

    Revert "Revert vehicles.json timezone conversion for now - it seems to be causing unacceptable slowness"
    This reverts commit dd233183a1410332fce0a3b6ae82ea879ac3f9e8.

--- vehicles/management/tests/test_bod_avl.py
+++ vehicles/management/tests/test_bod_avl.py
@@ -330,7 +330,7 @@ class BusOpenDataVehicleLocationsTest(TestCase):
                         "name": "T2-1 - DW18 HAM",
                     },
                     "heading": 92.0,
-                    "datetime": "2020-10-15T07:46:08Z",
+                    "datetime": "2020-10-15T08:46:08+01:00",
                     "destination": "",
                     "service_id": self.service_c.id,
                     "service": {"line_name": "C", "url": "/services/c"},
