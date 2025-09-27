f88a6a5de1c590293ef7afcb08924fc22b0f7964
Josh Goodwin <j@joshuagoodw.in>
2025-09-27 06:46:57 +0100

    test translink avl

--- vehicles/management/tests/test_newport.py
+++ vehicles/management/tests/test_newport.py
@@ -17,10 +17,7 @@ class NewportTest(TestCase):
         DataSource.objects.create(
             name="guernsey", url="https://example.com/api", settings={}
         )
-        # Region.objects.create(name="guernsey", id="GU")
         Operator.objects.create(noc="SGUE")
-        # s = Service.objects.create(current=True, region=r, line_name="47B", source=s)
-        # s.operator.add(o)
 
     def test(self):
         redis_client = fakeredis.FakeStrictRedis(version=7)
@@ -28,15 +25,25 @@ class NewportTest(TestCase):
         with (
             vcr.use_cassette(
                 str(Path(__file__).resolve().parent / "vcr" / "newport.yaml")
-            ),
+            ) as cassette,
             mock.patch(
                 "vehicles.management.import_live_vehicles.redis_client", redis_client
             ),
             mock.patch("vehicles.management.import_live_vehicles.sleep"),
             time_machine.travel("2025-09-24T06:30:00+00:00", tick=False),
-            self.assertNumQueries(25),
-            self.assertRaises(vcr.errors.CannotOverwriteExistingCassetteException),
         ):
-            call_command("newport", "--immediate")
+            with (
+                self.assertNumQueries(25),
+                self.assertRaises(vcr.errors.CannotOverwriteExistingCassetteException),
+            ):
+                call_command("newport", "--immediate")
+
+            cassette.rewind()
+
+            with (
+                self.assertNumQueries(2),
+                self.assertRaises(vcr.errors.CannotOverwriteExistingCassetteException),
+            ):
+                call_command("newport", "--immediate")
 
         self.assertEqual(VehicleJourney.objects.count(), 3)
---
+++ vehicles/management/tests/test_translink_avl.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from unittest import mock
+
+import fakeredis
+import vcr
+import time_machine
+from django.test import TestCase
+from django.core.management import call_command
+
+from busstops.models import Operator
+from ...models import VehicleJourney
+
+
+class TranslinkAVLTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Operator.objects.bulk_create(
+            [Operator(noc="ULB"), Operator(noc="MET"), Operator(noc="GDR")]
+        )
+
+    def test(self):
+        redis_client = fakeredis.FakeStrictRedis(version=7)
+
+        with (
+            vcr.use_cassette(
+                str(Path(__file__).resolve().parent / "vcr" / "translink_avl.yaml")
+            ) as cassette,
+            mock.patch(
+                "vehicles.management.import_live_vehicles.redis_client", redis_client
+            ),
+            mock.patch(
+                "vehicles.management.import_live_vehicles.sleep", side_effect=Exception
+            ),
+            time_machine.travel("2025-09-24T06:30:00+00:00", tick=False),
+        ):
+            with self.assertNumQueries(88), self.assertRaises(Exception):
+                call_command("import_translink_avl", "--immediate")
+
+            cassette.rewind()
+
+            with self.assertNumQueries(5), self.assertRaises(Exception):
+                call_command("import_translink_avl", "--immediate")
+
+        self.assertEqual(VehicleJourney.objects.count(), 8)
