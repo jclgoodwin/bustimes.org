1b47f925db16d7a8fe4f51db8a8df71302f67096
Josh Goodwin <j@joshuagoodw.in>
2025-10-04 15:07:01 +0100

    test (health check and db pinning) middleware

--- busstops/test_middleware.py
+++ busstops/test_middleware.py
@@ -1,10 +1,22 @@
-from django.test import TestCase
+from multidb.pinning import this_thread_is_pinned
+from http import HTTPStatus
+from django.test import TestCase, override_settings, modify_settings
 
 
-class WhiteNoiseWithFallbackMiddlewareTest(TestCase):
+class MiddlewareTest(TestCase):
+    @override_settings(ALLOWED_HOSTS=["bustimes.org"])
+    def test_health_check(self):
+        response = self.client.get("/")
+        # host header check is not allowed
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+
+        response = self.client.get("/up")
+        # host header check is bypassed
+        self.assertContains(response, "up!")
+
     def test_404_fallback(self):
         response = self.client.get("/static/css/style.p00p4nt5.css")
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         self.assertEqual(
             response.headers["cache-control"],
             "max-age=0, no-cache, no-store, must-revalidate, private",
@@ -12,8 +24,22 @@ class WhiteNoiseWithFallbackMiddlewareTest(TestCase):
 
     def test_404_no_fallback(self):
         response = self.client.get("/static/css/poo.p00p4nt5.css")
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         self.assertEqual(
             response.headers["cache-control"],
             "max-age=0, no-cache, no-store, must-revalidate, private",
         )
+
+    @modify_settings(
+        MIDDLEWARE={
+            "append": "busstops.middleware.pin_db_middleware",
+        }
+    )
+    def test_pin_db(self):
+        self.assertFalse(this_thread_is_pinned())
+
+        self.client.get("/admin/login/")
+        self.assertTrue(this_thread_is_pinned())
+
+        self.client.get("/")
+        self.assertFalse(this_thread_is_pinned())
