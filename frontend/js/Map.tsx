7076f47edeb1b7f5ff903a0feb0df2e00f1b221d
Josh Goodwin <j@joshuagoodw.in>
2025-11-06 12:10:27 +0000

    Revert "remove sentry js"
    This reverts commit 43762b1a7a4ebbd5228e3ad47fb27285911f4dcc.

--- frontend/js/Map.tsx
+++ frontend/js/Map.tsx
@@ -1,5 +1,6 @@
 import React, { memo, useEffect, createContext } from "react";
 import { createRoot } from "react-dom/client";
+// import { captureException } from "@sentry/react";
 
 import MapGL, {
   NavigationControl,
--- frontend/js/app.tsx
+++ frontend/js/app.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import React, { lazy } from "react";
 import { createRoot } from "react-dom/client";
 
@@ -8,6 +9,35 @@ import LoadingSorry from "./LoadingSorry";
 import ServiceMap from "./ServiceMap";
 const History = lazy(() => import("./History"));
 const MapRouter = lazy(() => import("./MapRouter"));
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://0d628b6fff45463bb803d045b99aa542@o55224.ingest.sentry.io/1379883",
+    allowUrls: [/bustimes\.org\/static\/js/, /bustimes\.org\/static\/dist\/js/],
+    ignoreErrors: [
+      // ignore errors in third-party advert code
+      "Load failed",
+      "Failed to fetch",
+      "AbortError: The user aborted a request",
+      "AbortError: Fetch is aborted",
+      "NetworkError when attempting to fetch resource",
+      "Non-Error promise rejection captured with value: undefined",
+      "from accessing a cross-origin frame. Protocols, domains, and ports must",
+      "Event `Event` (type=error) captured as promise rejection",
+      "this.kdmw is not a function",
+      "WKWebView API client did not respond to this postMessage",
+      "Origin https://bustimes.org is not allowed by Access-Control-Allow-Origin.",
+      "Failed to execute 'send' on 'XMLHttpRequest': Failed to load 'https://t.richaudience.com/",
+      "undefined is not an object (evaluating 'navigator.connection.effectiveType')",
+    ],
+    integrations: [
+      Sentry.globalHandlersIntegration({
+        onerror: true,
+        onunhandledrejection: false,
+      }),
+    ],
+  });
+}
 
 declare global {
   interface Window {
@@ -22,13 +52,17 @@ if (typeof window.globalThis === "undefined") {
   window.globalThis = window;
 }
 
+const error = <LoadingSorry text="Sorry, something has gone wrong" />;
+
 let rootElement: HTMLElement | null;
 if ((rootElement = document.getElementById("history"))) {
   // vehicle journey history
   const root = createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <History />
+      <Sentry.ErrorBoundary fallback={error}>
+        <History />
+      </Sentry.ErrorBoundary>
     </React.StrictMode>,
   );
 } else if (
@@ -38,17 +72,21 @@ if ((rootElement = document.getElementById("history"))) {
   const root = createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <ServiceMap
-        serviceId={window.SERVICE_ID}
-        buttonText={rootElement.innerText}
-      />
+      <Sentry.ErrorBoundary fallback={error}>
+        <ServiceMap
+          serviceId={window.SERVICE_ID}
+          buttonText={rootElement.innerText}
+        />
+      </Sentry.ErrorBoundary>
     </React.StrictMode>,
   );
 } else if ((rootElement = document.getElementById("hugemap"))) {
   const root = createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <MapRouter />
+      <Sentry.ErrorBoundary fallback={error}>
+        <MapRouter />
+      </Sentry.ErrorBoundary>
     </React.StrictMode>,
   );
 }
