03a25b9d9e80b01dc94718187dd50f2435199e0c
Josh Goodwin <j@joshuagoodw.in>
2025-11-09 16:39:47 +0000

    Revert "disable sentry js default integrations - less noise, still useful?"
    we want whichever integration was responsible user agent string (etc) information
    
    This reverts commit 8c438683573b6d0431994b48694975cbf16eeb31.

--- frontend/js/app.tsx
+++ frontend/js/app.tsx
@@ -14,7 +14,28 @@ if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn: "https://0d628b6fff45463bb803d045b99aa542@o55224.ingest.sentry.io/1379883",
     allowUrls: [/bustimes\.org\/static\/js/, /bustimes\.org\/static\/dist\/js/],
-    defaultIntegrations: false,
+    ignoreErrors: [
+      // ignore errors in third-party advert code
+      "Load failed",
+      "Failed to fetch",
+      "AbortError: The user aborted a request",
+      "AbortError: Fetch is aborted",
+      "NetworkError when attempting to fetch resource",
+      "Non-Error promise rejection captured with value: undefined",
+      "from accessing a cross-origin frame. Protocols, domains, and ports must",
+      "Event `Event` (type=error) captured as promise rejection",
+      "this.kdmw is not a function",
+      "WKWebView API client did not respond to this postMessage",
+      "Origin https://bustimes.org is not allowed by Access-Control-Allow-Origin.",
+      "Failed to execute 'send' on 'XMLHttpRequest': Failed to load 'https://t.richaudience.com/",
+      "undefined is not an object (evaluating 'navigator.connection.effectiveType')",
+    ],
+    integrations: [
+      Sentry.globalHandlersIntegration({
+        onerror: true,
+        onunhandledrejection: false,
+      }),
+    ],
   });
 }
 
