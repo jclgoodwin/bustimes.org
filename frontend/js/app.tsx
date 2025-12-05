497feb82fc667ae1159a318227479299d540d94f
Josh Goodwin <j@joshuagoodw.in>
2025-12-05 07:11:09 +0000

    respecify sentry allowUrls

--- frontend/js/app.tsx
+++ frontend/js/app.tsx
@@ -13,7 +13,7 @@ const MapRouter = lazy(() => import("./MapRouter"));
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn: "https://0d628b6fff45463bb803d045b99aa542@o55224.ingest.sentry.io/1379883",
-    // allowUrls: [/bustimes\.org\/static\/js/, /bustimes\.org\/static\/dist\/js/],
+    allowUrls: [/bustimes\.org/],
     integrations: [
       Sentry.globalHandlersIntegration({
         onerror: false,
