c477d3f9fd979801b2b0def0737695ac2b211a45
Josh Goodwin <j@joshuagoodw.in>
2026-01-26 17:58:44 +0000

    poppermost pages: remove unused annotations

--- busstops/tasks.py
+++ busstops/tasks.py
@@ -2,9 +2,6 @@ from django.core.cache import cache
 from huey import crontab
 from huey.contrib.djhuey import db_periodic_task
 
-from django.db.models import Value
-from django.db.models.aggregates import StringAgg
-
 from .views import operator_names
 from . import popular_pages
 
@@ -14,9 +11,6 @@ def update_popular_services():
     popular_services = (
         popular_pages.get_popular_services()
         .annotate(
-            line_names_str=StringAgg(
-                "route__line_name", Value("  "), default="line_name", distinct=True
-            ),
             operators=operator_names,
         )
         .order_by("?")
