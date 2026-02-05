b4bc60d7e92cca6fd26592f192726f0654dfed70
Josh Goodwin <j@joshuagoodw.in>
2026-02-05 17:47:19 +0000

    log Dundee journeys again

--- departures/live.py
+++ departures/live.py
@@ -237,7 +237,7 @@ def get_departures(stop, services, when) -> dict:
             if live_rows:
                 blend(departures, live_rows)
 
-                if source and source.name in ("Aberdeen", "Pembrokeshire", "SPT"):
+                if source:
                     # Record some information about the vehicle and journey,
                     # for enthusiasts,
                     # because the source doesn't support vehicle locations
