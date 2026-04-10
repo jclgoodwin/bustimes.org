9afe3a80afdf99d32608a6487abd90ed7266f6b2
Josh Goodwin <j@joshuagoodw.in>
2026-04-10 16:34:02 +0100

    disable the fancy drf HTML api render

--- buses/settings.py
+++ buses/settings.py
@@ -134,6 +134,7 @@ LOGIN_REDIRECT_URL = "/vehicles"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
+    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
 }
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
