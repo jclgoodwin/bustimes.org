cbeb1995eaf4371643ef1961c57accf0968165a3
Josh Goodwin <j@joshuagoodw.in>
2026-08-31 18:11:31 +0100

    add missing underground/metro GTFS route type

--- bustimes/gtfs_utils.py
+++ bustimes/gtfs_utils.py
@@ -20,6 +20,7 @@ logger = logging.getLogger(__name__)
 
 class RouteType(IntEnum):
     tram = 0
+    metro = 1
     rail = 2
     bus = 3
     ferry = 4
@@ -30,6 +31,7 @@ class RouteType(IntEnum):
 
 MODES = {
     RouteType.tram: "tram",
+    RouteType.metro: "metro",
     RouteType.rail: "rail",
     RouteType.bus: "bus",
     RouteType.ferry: "ferry",
